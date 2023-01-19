cdf8afa10c6d84d0393b1fd8bdd0654351706c37
Greg Pabian <35925521+grzpab@users.noreply.github.com>
2023-01-19 16:03:06 +0100

    wip

--- codemods/jscodeshift/react-router/6/remove-active-style/index.ts
+++ codemods/jscodeshift/react-router/6/remove-active-style/index.ts
@@ -39,48 +39,6 @@ function transform(
 
 	let dirtyFlag = false;
 
-	// const hasAttributes = (attributeFilter) => {
-	// 	const attributeNames = Object.keys(attributeFilter);
-	// 	return function filter(path) {
-	// 		if (!j.JSXElement.check(path.value)) {
-	// 			return false;
-	// 		}
-	// 		const elementAttributes = Object.create(null);
-	// 		path.value.openingElement.attributes.forEach(function (attr) {
-	// 			if (
-	// 				!j.JSXAttribute.check(attr) ||
-	// 				!(attr.name.name in attributeFilter)
-	// 			) {
-	// 				return;
-	// 			}
-	// 			elementAttributes[attr.name.name] = attr;
-	// 		});
-
-	// 		return attributeNames.every(function (name) {
-	// 			if (!(name in elementAttributes)) {
-	// 				return false;
-	// 			}
-
-	// 			const value = elementAttributes[name].value;
-	// 			const expected = attributeFilter[name];
-
-	// 			// Only when value is truthy access it's properties
-	// 			const actual = !value
-	// 				? value
-	// 				: j.Literal.check(value)
-	// 				? value.value
-	// 				: value.expression;
-
-	// 			if (typeof expected === 'function') {
-	// 				return expected(actual);
-	// 			}
-
-	// 			// Literal attribute values are always strings
-	// 			return String(expected) === actual;
-	// 		});
-	// 	};
-	// };
-
 	root.findJSXElements('NavLink')
 		// .filter(hasAttributes({ style: () => true, activeStyle: () => true }))
 		.forEach((path) => {
