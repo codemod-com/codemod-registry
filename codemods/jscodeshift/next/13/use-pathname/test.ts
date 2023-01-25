62275384d03f115b7ff7933e1b98ff34d6e27288
Greg Pabian <35925521+grzpab@users.noreply.github.com>
2023-01-25 15:50:05 +0100

    wip

--- codemods/jscodeshift/next/13/use-pathname/test.ts
+++ codemods/jscodeshift/next/13/use-pathname/test.ts
@@ -4,8 +4,38 @@ import assert from 'node:assert/strict';
 import { Context } from 'mocha';
 
 describe.only('next 13 use-pathname', function () {
-	it('should replace useRouter().pathname with usePathname()', async function (this: Context) {
+	it('should noop for pathname = a.b', async function (this: Context) {
+		const INPUT = 'const pathname = a.b;';
+
+		const fileInfo: FileInfo = {
+			path: 'index.js',
+			source: INPUT,
+		};
+
+		const actualOutput = transform(fileInfo, this.buildApi('js'), {});
+
+		assert.deepEqual(actualOutput, null);
+	});
+
+	it('should replace useRouter().pathname with usePathname() and add its import', async function (this: Context) {
 		const INPUT = 'const pathname = useRouter().pathname;';
+		const OUTPUT = `import { usePathname } from 'next/navigation';const pathname = usePathname();`;
+
+		const fileInfo: FileInfo = {
+			path: 'index.js',
+			source: INPUT,
+		};
+
+		const actualOutput = transform(fileInfo, this.buildApi('js'), {});
+
+		assert.deepEqual(
+			actualOutput?.replace(/\W/gm, ''),
+			OUTPUT.replace(/\W/gm, ''),
+		);
+	});
+
+	it('should replace router.pathname with usePathname() and add its import', async function (this: Context) {
+		const INPUT = 'const pathname = router.pathname;';
 		const OUTPUT = `import { usePathname } from 'next/navigation';const pathname = usePathname();`;
 
 		const fileInfo: FileInfo = {
