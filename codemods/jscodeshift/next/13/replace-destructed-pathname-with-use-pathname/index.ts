ea335b2879fb153455cf61aaa5c09d1c999f728a
Greg Pabian <35925521+grzpab@users.noreply.github.com>
2023-01-26 14:15:48 +0100

    wip

--- codemods/jscodeshift/next/13/replace-destructed-pathname-with-use-pathname/index.ts
+++ codemods/jscodeshift/next/13/replace-destructed-pathname-with-use-pathname/index.ts
@@ -1,38 +1,54 @@
 import { API, FileInfo, ObjectPattern, Options, Transform } from 'jscodeshift';
 
-const buildProxy = <T extends object>(obj: T) => {
+const buildProxy = <T extends object>(obj: T, onDirty: () => void) => {
 	let dirtyFlag = false;
 
 	const proxy = new Proxy(obj, {
 		get(target, prop, receiver) {
 			if (prop === 'replace' || prop === 'insertAfter') {
-				dirtyFlag = true;
+				if (!dirtyFlag) {
+					dirtyFlag = true;
+					onDirty();
+				}
 			}
 			return Reflect.get(target, prop, receiver);
 		},
 	});
 
-	return [proxy, () => dirtyFlag] as const;
+	return proxy;
 };
+
+type DirtyFlag = 'objectPattern' | 'variableDeclaration';
 
 export default function transformer(
 	file: FileInfo,
 	api: API,
 	options: Options,
 ) {
+	let dirtyFlags = new Set<DirtyFlag>();
+
+	const buildOnDirty = (value: DirtyFlag) => () => {
+		dirtyFlags.add(value);
+	};
+
 	const j = api.jscodeshift;
 	const root = j(file.source);
 
 	let variableDeclarationDirtyFlag = false;
 
-	root.find(j.VariableDeclaration).forEach((variableDeclaration) => {
-		let dirtyFlag = false;
+	root.find(j.VariableDeclaration).forEach((variableDeclarationPath) => {
+		const variableDeclaration = buildProxy(
+			variableDeclarationPath,
+			buildOnDirty('variableDeclaration'),
+		);
 
 		j(variableDeclaration)
 			.find(j.ObjectPattern)
 			.forEach((objectPatternPath) => {
-				const [objectPattern, getDirtyFlag] =
-					buildProxy(objectPatternPath);
+				const objectPattern = buildProxy(
+					objectPatternPath,
+					buildOnDirty('objectPattern'),
+				);
 
 				let keyName: string | null = null;
 				const properties: ObjectPattern['properties'] = [];
@@ -57,12 +73,10 @@ export default function transformer(
 
 				if (keyName) {
 					objectPattern.replace(j.objectPattern(properties));
-
-					dirtyFlag = true;
 				}
 			});
 
-		if (dirtyFlag) {
+		if (dirtyFlags.has('objectPattern')) {
 			variableDeclaration.insertAfter(
 				j.variableDeclaration('const', [
 					j.variableDeclarator(
