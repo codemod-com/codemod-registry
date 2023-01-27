5ebeae4f1fc24a4455b067e9a244b43c849f748e
Greg Pabian <35925521+grzpab@users.noreply.github.com>
2023-01-27 12:25:40 +0100

    wip

--- codemods/jscodeshift/next/13/replace-use-router-query-with-use-search-params/index.ts
+++ codemods/jscodeshift/next/13/replace-use-router-query-with-use-search-params/index.ts
@@ -1,4 +1,11 @@
-import { ImportSpecifier, Project, SyntaxKind } from 'ts-morph';
+import {
+	BindingElement,
+	ImportSpecifier,
+	ObjectBindingPattern,
+	Project,
+	SyntaxKind,
+} from 'ts-morph';
+import { factory } from 'typescript';
 
 export default function transformer(sourceFileText: string): string {
 	const project = new Project();
@@ -6,6 +13,7 @@ export default function transformer(sourceFileText: string): string {
 	const sourceFile = project.createSourceFile('index.ts', sourceFileText);
 
 	const importSpecifiers: ImportSpecifier[] = [];
+	const objectBindingPatterns = new Set<ObjectBindingPattern>();
 
 	sourceFile.getImportDeclarations().forEach((declaration) => {
 		declaration
@@ -23,23 +31,40 @@ export default function transformer(sourceFileText: string): string {
 		.forEach((callExpression) => {
 			const expression = callExpression.getExpression();
 
-			if (
-				expression.isKind(SyntaxKind.Identifier) &&
-				expression.getText() === 'useRouter'
-			) {
-				console.log('HERE');
-
-				const variableDeclaration = callExpression.getParentIfKind(
-					SyntaxKind.VariableDeclaration,
-				);
-
-				if 
+			if (!expression.isKind(SyntaxKind.Identifier)) {
+				return;
 			}
+
+			if (expression.getText() !== 'useRouter') {
+				return;
+			}
+
+			const variableDeclaration = callExpression.getParentIfKind(
+				SyntaxKind.VariableDeclaration,
+			);
+
+			if (!variableDeclaration) {
+				return;
+			}
+
+			variableDeclaration
+				.getDescendantsOfKind(SyntaxKind.ObjectBindingPattern)
+				.forEach((objectBindingPattern) => {
+					objectBindingPattern
+						.getElements()
+						.forEach((bindingElement) => {
+							if (bindingElement.getName() === 'query') {
+								objectBindingPatterns.add(objectBindingPattern);
+							}
+						});
+				});
 		});
 
 	// removal
-
 	importSpecifiers.forEach((importSpecifier) => importSpecifier.remove());
+	objectBindingPatterns.forEach((bindingElement) =>
+		bindingElement.transform(() => factory.createObjectBindingPattern([])),
+	);
 
 	return sourceFile.getText();
 }
