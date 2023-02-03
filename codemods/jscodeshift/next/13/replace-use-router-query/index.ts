f852658bc322c7c66e1b85632f4e70ea2686ea37
Greg Pabian <35925521+grzpab@users.noreply.github.com>
2023-02-03 17:40:33 +0100

    support the more advanced case of useSearchParams migration (#11)
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * fix
    
    * wip
    
    * wip
    
    * fix
    
    * wykop
    
    * wip
    
    * wip

---
+++ codemods/jscodeshift/next/13/replace-use-router-query/index.ts
@@ -0,0 +1,554 @@
+import {
+	API,
+	Collection,
+	FileInfo,
+	Options,
+	JSCodeshift,
+	ImportDeclaration,
+	VariableDeclarator,
+	MemberExpression,
+	CallExpression,
+	SpreadElement,
+} from 'jscodeshift';
+
+type IntuitaTransform = (j: API['jscodeshift'], root: Collection<any>) => void;
+
+const findImportDeclarations =
+	(importedName: string, sourceValue: string) =>
+	(j: JSCodeshift, root: Collection<any>): Collection<ImportDeclaration> => {
+		return root.find(j.ImportDeclaration, {
+			specifiers: [
+				{
+					imported: {
+						type: 'Identifier',
+						name: importedName,
+					},
+				},
+			],
+			source: {
+				value: sourceValue,
+			},
+		});
+	};
+
+const findVariableDeclaratorWithCallExpression =
+	(calleeName: string) =>
+	(j: JSCodeshift, root: Collection<any>): Collection<VariableDeclarator> => {
+		return root.find(j.VariableDeclarator, {
+			init: {
+				type: 'CallExpression',
+				callee: {
+					type: 'Identifier',
+					name: calleeName,
+				},
+			},
+		});
+	};
+
+const findMemberExpressionsWithCallExpression =
+	(objectCalleeName: string, propertyName: string) =>
+	(j: JSCodeshift, root: Collection<any>): Collection<MemberExpression> => {
+		return root.find(j.MemberExpression, {
+			object: {
+				type: 'CallExpression',
+				callee: {
+					type: 'Identifier',
+					name: objectCalleeName,
+				},
+			},
+			property: {
+				type: 'Identifier',
+				name: propertyName,
+			},
+		});
+	};
+
+const findCallExpressionsWithMemberExpression =
+	(calleeObjectName: string, calleePropertyName: string) =>
+	(j: JSCodeshift, root: Collection<any>): Collection<CallExpression> => {
+		return root.find(j.CallExpression, {
+			callee: {
+				type: 'MemberExpression',
+				object: {
+					type: 'Identifier',
+					name: calleeObjectName,
+				},
+				property: {
+					type: 'Identifier',
+					name: calleePropertyName,
+				},
+			},
+		});
+	};
+
+const findMemberExpressions =
+	(objectName: string, propertyName: string | undefined) =>
+	(j: JSCodeshift, root: Collection<any>): Collection<MemberExpression> => {
+		type FilterType = Parameters<
+			typeof root.find<MemberExpression>
+		>[1] & {};
+
+		const filter: FilterType = {
+			object: {
+				type: 'Identifier' as const,
+				name: objectName,
+			},
+			property: {
+				type: 'Identifier' as const,
+			},
+		};
+
+		if (propertyName) {
+			filter.property = {
+				type: 'Identifier' as const,
+				name: propertyName,
+			};
+		}
+
+		return root.find(j.MemberExpression, filter);
+	};
+
+const findVariableDeclaratorWithObjectPatternAndCallExpression =
+	(idPropertiesKeyName: string, initCalleeName: string) =>
+	(j: JSCodeshift, root: Collection<any>): Collection<VariableDeclarator> => {
+		return root.find(j.VariableDeclarator, {
+			id: {
+				type: 'ObjectPattern',
+				properties: [
+					{
+						type: 'ObjectProperty',
+						key: {
+							type: 'Identifier',
+							name: idPropertiesKeyName,
+						},
+					},
+				],
+			},
+			init: {
+				type: 'CallExpression',
+				callee: {
+					type: 'Identifier',
+					name: initCalleeName,
+				},
+				arguments: [],
+			},
+		});
+	};
+
+const findCallExpressions =
+	(calleeName: string) =>
+	(j: JSCodeshift, root: Collection<any>): Collection<CallExpression> => {
+		return root.find(j.CallExpression, {
+			callee: {
+				type: 'Identifier',
+				name: calleeName,
+			},
+		});
+	};
+
+const findSpreadElements =
+	(argumentObjectName: string, argumentPropertyQuery: string) =>
+	(j: JSCodeshift, root: Collection<any>): Collection<SpreadElement> => {
+		return root.find(j.SpreadElement, {
+			argument: {
+				type: 'MemberExpression',
+				object: {
+					type: 'Identifier',
+					name: argumentObjectName,
+				},
+				property: {
+					type: 'Identifier',
+					name: argumentPropertyQuery,
+				},
+			},
+		});
+	};
+
+export const transformAddUseSearchParamsImport: IntuitaTransform = (
+	j: API['jscodeshift'],
+	root: Collection<any>,
+): void => {
+	const importDeclarations = findImportDeclarations(
+		'useRouter',
+		'next/router',
+	)(j, root);
+
+	if (importDeclarations.size() === 0) {
+		return;
+	}
+
+	let hasQueries = false;
+
+	root.find(j.BlockStatement).forEach((blockStatementPath) => {
+		const blockStatement = j(blockStatementPath);
+
+		// 1
+		const routerNames: string[] = [];
+
+		findVariableDeclaratorWithCallExpression('useRouter')(
+			j,
+			blockStatement,
+		).forEach((variableDeclaratorPath) => {
+			const { id } = variableDeclaratorPath.node;
+
+			if (id.type !== 'Identifier') {
+				return;
+			}
+
+			routerNames.push(id.name);
+		});
+
+		for (const routerName of routerNames) {
+			if (
+				findMemberExpressions(routerName, 'query')(
+					j,
+					blockStatement,
+				).size() > 0
+			) {
+				hasQueries = true;
+				return;
+			}
+		}
+
+		// 2
+		if (
+			findMemberExpressionsWithCallExpression('useRouter', 'query')(
+				j,
+				blockStatement,
+			).size() > 0
+		) {
+			hasQueries = true;
+			return;
+		}
+
+		// 3
+		if (
+			findVariableDeclaratorWithObjectPatternAndCallExpression(
+				'query',
+				'useRouter',
+			)(j, root).size() > 0
+		) {
+			hasQueries = true;
+			return;
+		}
+	});
+
+	if (!hasQueries) {
+		return;
+	}
+
+	const importDeclaration = j.importDeclaration(
+		[
+			j.importSpecifier(
+				j.identifier('useSearchParams'),
+				j.identifier('useSearchParams'),
+			),
+		],
+		j.stringLiteral('next/navigation'),
+	);
+
+	root.find(j.Program).forEach((program) => {
+		program.value.body.unshift(importDeclaration);
+	});
+};
+
+export const transformAddSearchParamsVariableDeclarator: IntuitaTransform = (
+	j,
+	root,
+): void => {
+	const importDeclarations = findImportDeclarations(
+		'useRouter',
+		'next/router',
+	)(j, root);
+
+	if (importDeclarations.size() === 0) {
+		return;
+	}
+
+	root.find(j.BlockStatement).forEach((blockStatementPath) => {
+		const blockStatement = j(blockStatementPath);
+
+		if (findCallExpressions('useRouter')(j, blockStatement).size() === 0) {
+			return;
+		}
+
+		blockStatementPath.node.body.unshift(
+			j.variableDeclaration('const', [
+				j.variableDeclarator(
+					j.identifier('searchParams'),
+					j.callExpression(j.identifier('useSearchParams'), []),
+				),
+			]),
+		);
+	});
+};
+
+export const transformTripleDotReplaceRouterQueryWithSearchParams: IntuitaTransform =
+	(j, root): void => {
+		const importDeclarations = findImportDeclarations(
+			'useRouter',
+			'next/router',
+		)(j, root);
+
+		if (importDeclarations.size() === 0) {
+			return;
+		}
+
+		root.find(j.BlockStatement).forEach((blockStatementPath) => {
+			const blockStatement = j(blockStatementPath);
+
+			const routerNames: string[] = [];
+
+			findVariableDeclaratorWithCallExpression('useRouter')(
+				j,
+				blockStatement,
+			).forEach((variableDeclaratorPath) => {
+				const { id } = variableDeclaratorPath.node;
+
+				if (!id || id.type !== 'Identifier') {
+					return;
+				}
+
+				routerNames.push(id.name);
+			});
+
+			for (const routerName of routerNames) {
+				findSpreadElements(routerName, 'query')(
+					j,
+					blockStatement,
+				).replaceWith(() => {
+					return j.spreadElement(
+						j.callExpression(
+							j.memberExpression(
+								j.identifier('searchParams'),
+								j.identifier('entries'),
+								false,
+							),
+							[],
+						),
+					);
+				});
+			}
+		});
+	};
+
+export const transformReplaceRouterQueryWithSearchParams: IntuitaTransform = (
+	j,
+	root,
+): void => {
+	const importDeclarations = findImportDeclarations(
+		'useRouter',
+		'next/router',
+	)(j, root);
+
+	if (importDeclarations.size() === 0) {
+		return;
+	}
+
+	root.find(j.BlockStatement).forEach((blockStatementPath) => {
+		const blockStatement = j(blockStatementPath);
+
+		const routerNames: string[] = [];
+
+		findVariableDeclaratorWithCallExpression('useRouter')(
+			j,
+			blockStatement,
+		).forEach((variableDeclaratorPath) => {
+			const { id } = variableDeclaratorPath.node;
+
+			if (!id || id.type !== 'Identifier') {
+				return;
+			}
+
+			routerNames.push(id.name);
+		});
+
+		for (const routerName of routerNames) {
+			findMemberExpressions(routerName, 'query')(
+				j,
+				blockStatement,
+			).replaceWith(() => j.identifier('searchParams'));
+		}
+	});
+};
+
+export const transformUseRouterQueryWithUseSearchParams: IntuitaTransform = (
+	j,
+	root,
+): void => {
+	const importDeclarations = findImportDeclarations(
+		'useRouter',
+		'next/router',
+	)(j, root);
+
+	if (importDeclarations.size() === 0) {
+		return;
+	}
+
+	findMemberExpressionsWithCallExpression('useRouter', 'query')(
+		j,
+		root,
+	).replaceWith(() => j.callExpression(j.identifier('useSearchParams'), []));
+};
+
+export const transformReplaceSearchParamsXWithSearchParamsGetX: IntuitaTransform =
+	(j, root): void => {
+		const importDeclarations = findImportDeclarations(
+			'useSearchParams',
+			'next/navigation',
+		)(j, root);
+
+		if (importDeclarations.size() === 0) {
+			return;
+		}
+
+		const variableNames: string[] = [];
+
+		findVariableDeclaratorWithCallExpression('useSearchParams')(
+			j,
+			root,
+		).forEach((variableDeclaratorPath) => {
+			const { id } = variableDeclaratorPath.node;
+
+			if (id.type !== 'Identifier') {
+				return;
+			}
+
+			variableNames.push(id.name);
+		});
+
+		for (const variableName of variableNames) {
+			findMemberExpressions(variableName, undefined)(j, root).replaceWith(
+				(memberExpressionPath) => {
+					const { property } = memberExpressionPath.node;
+
+					if (
+						property.type !== 'Identifier' ||
+						property.name === 'entries'
+					) {
+						return memberExpressionPath.node;
+					}
+
+					return j.callExpression(
+						j.memberExpression(
+							j.identifier('searchParams'),
+							j.identifier('get'),
+							false,
+						),
+						[memberExpressionPath.node.property],
+					);
+				},
+			);
+		}
+	};
+
+export const transformReplaceUseMemoSecondArgumentWithSearchParams: IntuitaTransform =
+	(j, root): void => {
+		const importDeclarations = findImportDeclarations(
+			'useSearchParams',
+			'next/navigation',
+		)(j, root);
+
+		if (importDeclarations.size() === 0) {
+			return;
+		}
+
+		root.find(j.BlockStatement).forEach((blockStatementPath) => {
+			const blockStatement = j(blockStatementPath);
+
+			const variableNames: string[] = [];
+
+			findVariableDeclaratorWithCallExpression('useSearchParams')(
+				j,
+				blockStatement,
+			).forEach((variableDeclaratorPath) => {
+				const { id } = variableDeclaratorPath.node;
+
+				if (id.type !== 'Identifier') {
+					return;
+				}
+
+				variableNames.push(id.name);
+			});
+
+			const hadSearchParamsGetsPerCall: boolean[] = [];
+
+			findCallExpressions('useMemo')(j, blockStatement).forEach(
+				(callExpressionPath, i) => {
+					const { arguments: args } = callExpressionPath.value;
+
+					if (!args[1]) {
+						return;
+					}
+
+					const dependencyArguments = j(args[1]);
+
+					let hadSearchParamsGets = false;
+
+					for (const variableName of variableNames) {
+						findCallExpressionsWithMemberExpression(
+							variableName,
+							'get',
+						)(j, dependencyArguments)
+							.forEach(() => {
+								hadSearchParamsGets = true;
+							})
+							.remove();
+					}
+
+					hadSearchParamsGetsPerCall[i] = hadSearchParamsGets;
+				},
+			);
+
+			findCallExpressions('useMemo')(j, blockStatement)
+				.filter((_, i) => {
+					return hadSearchParamsGetsPerCall[i] ?? false;
+				})
+				.replaceWith((callExpression) => {
+					const { arguments: arg } = callExpression.node;
+
+					if (
+						!arg[0] ||
+						!arg[1] ||
+						arg[1].type !== 'ArrayExpression'
+					) {
+						return callExpression.node;
+					}
+
+					return j.callExpression(callExpression.node.callee, [
+						arg[0],
+						j.arrayExpression([
+							...variableNames.map((variableName) =>
+								j.identifier(variableName),
+							),
+							...arg[1].elements,
+						]),
+					]);
+				});
+		});
+	};
+
+export default function transformer(
+	file: FileInfo,
+	api: API,
+	options: Options,
+) {
+	const transforms: IntuitaTransform[] = [
+		transformAddUseSearchParamsImport,
+		transformAddSearchParamsVariableDeclarator,
+		transformTripleDotReplaceRouterQueryWithSearchParams,
+		transformReplaceRouterQueryWithSearchParams,
+		transformUseRouterQueryWithUseSearchParams,
+		transformReplaceSearchParamsXWithSearchParamsGetX,
+		transformReplaceUseMemoSecondArgumentWithSearchParams,
+	];
+
+	const j = api.jscodeshift;
+	const root = j(file.source);
+
+	for (const intuitaTransform of transforms) {
+		intuitaTransform(j, root);
+	}
+
+	return root.toSource();
+}
---
+++ codemods/jscodeshift/next/13/replace-use-router-query/test.ts
@@ -0,0 +1,319 @@
+import transform, {
+	transformAddSearchParamsVariableDeclarator,
+	transformAddUseSearchParamsImport,
+	transformReplaceRouterQueryWithSearchParams,
+	transformReplaceSearchParamsXWithSearchParamsGetX,
+	transformTripleDotReplaceRouterQueryWithSearchParams,
+	transformUseRouterQueryWithUseSearchParams,
+} from '.';
+import assert from 'node:assert/strict';
+import { Context } from 'mocha';
+import { FileInfo } from 'jscodeshift';
+
+describe('next 13 replace-use-router-query', function () {
+	it('should add useSearchParams import because of "router.query"', async function (this: Context) {
+		const { jscodeshift } = this.buildApi('tsx');
+
+		const root = jscodeshift(`
+			import { useRouter } from 'next/router';
+
+			function Component() {
+				const router = useRouter();
+
+				const x = router.query.a;
+			}
+		`);
+
+		transformAddUseSearchParamsImport(jscodeshift, root);
+
+		assert.deepEqual(
+			root?.toSource().replace(/\W/gm, '') ?? '',
+			`
+			import { useSearchParams } from 'next/navigation';
+			import { useRouter } from 'next/router';
+
+			function Component() {
+				const router = useRouter();
+
+				const x = router.query.a;
+			}
+			`.replace(/\W/gm, ''),
+		);
+	});
+
+	it('should add useSearchParams import because of "useRouter().query"', async function (this: Context) {
+		const { jscodeshift } = this.buildApi('tsx');
+
+		const root = jscodeshift(`
+			import { useRouter } from 'next/router';
+
+			function Component() {
+				const a = useRouter().query.a;
+			}
+		`);
+
+		transformAddUseSearchParamsImport(jscodeshift, root);
+
+		assert.deepEqual(
+			root?.toSource().replace(/\W/gm, '') ?? '',
+			`
+			import { useSearchParams } from 'next/navigation';
+			import { useRouter } from 'next/router';
+
+			function Component() {
+				const a = useRouter().query.a;
+			}
+			`.replace(/\W/gm, ''),
+		);
+	});
+
+	it('should add useSearchParams import because of "const { query } = useRouter()"', async function (this: Context) {
+		const { jscodeshift } = this.buildApi('tsx');
+
+		const root = jscodeshift(`
+			import { useRouter } from 'next/router';
+
+			function Component() {
+				const { query } = useRouter();
+			}
+		`);
+
+		transformAddUseSearchParamsImport(jscodeshift, root);
+
+		assert.deepEqual(
+			root?.toSource().replace(/\W/gm, '') ?? '',
+			`
+			import { useSearchParams } from 'next/navigation';
+			import { useRouter } from 'next/router';
+
+			function Component() {
+				const { query } = useRouter();
+			}
+			`.replace(/\W/gm, ''),
+		);
+	});
+
+	it('should add searchParams variable declarator because of "useRouter()"', async function (this: Context) {
+		const { jscodeshift } = this.buildApi('tsx');
+
+		const root = jscodeshift(`
+			import { useRouter } from 'next/router';
+
+			function Component() {
+				const { query } = useRouter();
+			}
+		`);
+
+		transformAddSearchParamsVariableDeclarator(jscodeshift, root);
+
+		assert.deepEqual(
+			root?.toSource().replace(/\W/gm, '') ?? '',
+			`
+			import { useRouter } from 'next/router';
+
+			function Component() {
+				const searchParams = useSearchParams();
+				const { query } = useRouter();
+			}
+			`.replace(/\W/gm, ''),
+		);
+	});
+
+	it('should replace "...?.query" with "...searchParams.entries()."', async function (this: Context) {
+		const { jscodeshift } = this.buildApi('tsx');
+
+		const root = jscodeshift(`
+			import { useRouter } from 'next/router';
+
+			function Component() {
+				const r = useRouter();
+
+				const shallowCopiedQuery = { ...r.query }
+			}
+		`);
+
+		transformTripleDotReplaceRouterQueryWithSearchParams(jscodeshift, root);
+
+		assert.deepEqual(
+			root?.toSource().replace(/\W/gm, '') ?? '',
+			`
+			import { useRouter } from 'next/router';
+
+			function Component() {
+				const r = useRouter();
+
+				const shallowCopiedQuery = { ...searchParams.entries() }
+			}
+			`.replace(/\W/gm, ''),
+		);
+	});
+
+	it('should replace "?.query" with "searchParams"', async function (this: Context) {
+		const { jscodeshift } = this.buildApi('tsx');
+
+		const root = jscodeshift(`
+			import { useRouter } from 'next/router';
+
+			function Component() {
+				const r = useRouter();
+
+				const a = r.query.a;
+			}
+		`);
+
+		transformReplaceRouterQueryWithSearchParams(jscodeshift, root);
+
+		assert.deepEqual(
+			root?.toSource().replace(/\W/gm, '') ?? '',
+			`
+			import { useRouter } from 'next/router';
+
+			function Component() {
+				const r = useRouter();
+
+				const a = searchParams.a;
+			}
+			`.replace(/\W/gm, ''),
+		);
+	});
+
+	it('should replace "useRouter().query" with "useSearchParams()"', async function (this: Context) {
+		const { jscodeshift } = this.buildApi('tsx');
+
+		const root = jscodeshift(`
+			import { useRouter } from 'next/router';
+
+			function Component() {
+				const a = useRouter().query.a;
+			}
+		`);
+
+		transformUseRouterQueryWithUseSearchParams(jscodeshift, root);
+
+		assert.deepEqual(
+			root?.toSource().replace(/\W/gm, '') ?? '',
+			`
+			import { useRouter } from 'next/router';
+
+			function Component() {
+				const a = useSearchParams().a;
+			}
+			`.replace(/\W/gm, ''),
+		);
+	});
+
+	it('should replace "searchParams.a" with "searchParams.get("a")"', async function (this: Context) {
+		const { jscodeshift } = this.buildApi('tsx');
+
+		const root = jscodeshift(`
+			import { useSearchParams } from 'next/navigation';
+
+			function Component() {
+				const searchParams = useSearchParams();
+
+				const a = searchParams.a;
+			}
+		`);
+
+		transformReplaceSearchParamsXWithSearchParamsGetX(jscodeshift, root);
+
+		assert.deepEqual(
+			root?.toSource().replace(/\W/gm, '') ?? '',
+			`
+			import { useSearchParams } from 'next/navigation';
+
+			function Component() {
+				const searchParams = useSearchParams();
+
+				const a = searchParams.get('a');
+			}
+			`.replace(/\W/gm, ''),
+		);
+	});
+
+	it('should replace INPUT with OUTPUT', async function (this: Context) {
+		const INPUT = `
+			import { useRouter } from 'next/router';
+
+			function Component() {
+				const router = useRouter();
+				
+				const x = router.query.a;
+
+				const z = { ...router.query, b: 1 }
+			}
+		`;
+
+		const OUTPUT = `
+			import { useSearchParams } from 'next/navigation';
+			import { useRouter } from 'next/router';
+
+			function Component() {
+				const searchParams = useSearchParams();
+				const router = useRouter();
+				
+				const x = searchParams.get('a');
+
+				const z = { ...searchParams.entries(), b: 1}
+			}
+		`;
+
+		const fileInfo: FileInfo = {
+			path: 'index.js',
+			source: INPUT,
+		};
+
+		const actualOutput = transform(fileInfo, this.buildApi('js'), {});
+
+		assert.deepEqual(
+			actualOutput?.replace(/\W/gm, ''),
+			OUTPUT.replace(/\W/gm, ''),
+		);
+	});
+
+	it('should replace INPUT with OUTPUT (2)', async function (this: Context) {
+		const INPUT = `
+			import { useRouter } from 'next/router';
+
+			const a = 1;
+
+			function Component() {
+				const router = useRouter();
+
+				const nextA = useMemo(
+					() => (router.query.a ? null : router.query.b),
+					[router.query.a, router.query.b, c],
+				) ?? a;
+			}
+		`;
+
+		const OUTPUT = `
+			import { useSearchParams } from 'next/navigation';
+			import { useRouter } from 'next/router';
+
+			const a = 1;
+
+			function Component() {
+				const searchParams = useSearchParams()
+				const router = useRouter();
+
+				const nextA = useMemo(
+					() => (searchParams.get('a') ? null : searchParams.get('b')),
+					[searchParams, c],
+				) ?? a;
+			}
+		`;
+
+		const fileInfo: FileInfo = {
+			path: 'index.js',
+			source: INPUT,
+		};
+
+		const actualOutput = transform(fileInfo, this.buildApi('tsx'), {});
+
+		assert.deepEqual(
+			actualOutput?.replace(/\W/gm, ''),
+			OUTPUT.replace(/\W/gm, ''),
+		);
+	});
+});
