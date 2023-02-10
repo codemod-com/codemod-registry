3cb637fdd21e66d0f058586c9138f6c465be7326
Greg Pabian <35925521+grzpab@users.noreply.github.com>
2023-02-10 10:33:44 +0100

    wip

--- codemods/jscodeshift/next/13/replace-use-router-query/test.ts
+++ codemods/jscodeshift/next/13/replace-use-router-query/test.ts
@@ -490,4 +490,51 @@ describe('next 13 replace-use-router-query', function () {
 			OUTPUT.replace(/\W/gm, ''),
 		);
 	});
+
+	it('should noop for pathname = a.b', async function (this: Context) {
+		const INPUT = 'const pathname = a.b;';
+
+		const fileInfo: FileInfo = {
+			path: 'index.js',
+			source: INPUT,
+		};
+
+		const actualOutput = transform(fileInfo, this.buildApi('js'), {});
+
+		assert.deepEqual(actualOutput, INPUT);
+	});
+
+	it('should replace useRouter().pathname with usePathname()', async function (this: Context) {
+		const INPUT = 'const pathname = useRouter().pathname;';
+		const OUTPUT = 'const pathname = usePathname();';
+
+		const fileInfo: FileInfo = {
+			path: 'index.js',
+			source: INPUT,
+		};
+
+		const actualOutput = transform(fileInfo, this.buildApi('js'), {});
+
+		assert.deepEqual(
+			actualOutput?.replace(/\W/gm, ''),
+			OUTPUT.replace(/\W/gm, ''),
+		);
+	});
+
+	it('should replace router.pathname with usePathname()', async function (this: Context) {
+		const INPUT = 'const pathname = router.pathname;';
+		const OUTPUT = 'const pathname = usePathname();';
+
+		const fileInfo: FileInfo = {
+			path: 'index.js',
+			source: INPUT,
+		};
+
+		const actualOutput = transform(fileInfo, this.buildApi('js'), {});
+
+		assert.deepEqual(
+			actualOutput?.replace(/\W/gm, ''),
+			OUTPUT.replace(/\W/gm, ''),
+		);
+	});
 });
