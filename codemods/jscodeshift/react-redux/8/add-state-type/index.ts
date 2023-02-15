ce480985410227f6f356aedc6944d8988a54894b
Greg Pabian <35925521+grzpab@users.noreply.github.com>
2023-02-15 12:01:41 +0100

    wip

--- codemods/jscodeshift/react-redux/8/add-state-type/index.ts
+++ codemods/jscodeshift/react-redux/8/add-state-type/index.ts
@@ -24,8 +24,6 @@ export const upsertTypeAnnotationOnStateIdentifier: AtomicMod = (
 	root,
 	settings,
 ) => {
-	console.log('upsertTypeAnnotationOnStateIndentifier');
-
 	let dirtyFlag: boolean = false;
 
 	if (!root.isOfType(j.ArrowFunctionExpression)) {
