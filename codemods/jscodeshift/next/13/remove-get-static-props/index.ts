e645ec1741167a184f5206e3495b4a2d0ef9168e
Greg Pabian <35925521+grzpab@users.noreply.github.com>
2023-02-24 13:47:05 +0100

    wip

--- codemods/jscodeshift/next/13/remove-get-static-props/index.ts
+++ codemods/jscodeshift/next/13/remove-get-static-props/index.ts
@@ -1,11 +1,63 @@
-import type { API, FileInfo, Options, Transform } from 'jscodeshift';
+import type {
+	API,
+	Collection,
+	FileInfo,
+	JSCodeshift,
+	Options,
+	Transform,
+} from 'jscodeshift';
+
+type ModFunction<T, D extends 'read' | 'write'> = (
+	j: JSCodeshift,
+	root: Collection<T>,
+	settings: Partial<Record<string, string>>,
+) => [D extends 'write' ? boolean : false, ReadonlyArray<LazyModFunction>];
+
+type LazyModFunction = [
+	ModFunction<any, 'read' | 'write'>,
+	Collection<any>,
+	Partial<Record<string, string>>,
+];
 
 export default function transform(
 	file: FileInfo,
 	api: API,
 	options: Options,
 ): string | undefined {
-	return undefined;
+	const j = api.jscodeshift;
+
+	let dirtyFlag = false;
+
+	const root = j(file.source);
+	const settings = {};
+
+	const lazyModFunctions: LazyModFunction[] = [];
+
+	const handleLazyModFunction = (lazyModFunction: LazyModFunction) => {
+		const [modFunction, localCollection, localSettings] = lazyModFunction;
+
+		const [localDirtyFlag, localLazyModFunctions] = modFunction(
+			j,
+			localCollection,
+			localSettings,
+		);
+
+		dirtyFlag ||= localDirtyFlag;
+
+		for (const localLazyModFunction of localLazyModFunctions) {
+			handleLazyModFunction(localLazyModFunction);
+		}
+	};
+
+	for (const lazyModFunction of lazyModFunctions) {
+		handleLazyModFunction(lazyModFunction);
+	}
+
+	if (!dirtyFlag) {
+		return undefined;
+	}
+
+	return root.toSource();
 }
 
 transform satisfies Transform;
