c7a62378cff942d6a296a6b511c589d676a8c24d
Greg Pabian <35925521+grzpab@users.noreply.github.com>
2023-04-18 20:47:05 +0200

    handle const { query } = useRouter() cases

--- codemods/ts-morph/next/13/replace-next-router/index.ts
+++ codemods/ts-morph/next/13/replace-next-router/index.ts
@@ -1,243 +1,290 @@
-import { EmitHint, ImportDeclaration } from "ts-morph";
-import { PropertyAccessExpression, SourceFile, ts } from "ts-morph";
-import { Node } from "ts-morph";
+import { EmitHint, ImportDeclaration } from 'ts-morph';
+import { PropertyAccessExpression, SourceFile, ts } from 'ts-morph';
+import { Node } from 'ts-morph';
 
 export const buildContainer = <T>(initialValue: NonNullable<T>) => {
-  let currentValue: NonNullable<T> = initialValue;
+	let currentValue: NonNullable<T> = initialValue;
 
-  const get = (): NonNullable<T> => {
-    return currentValue;
-  };
+	const get = (): NonNullable<T> => {
+		return currentValue;
+	};
 
-  const set = (value: NonNullable<T>): void => {
-    currentValue = value;
-  };
+	const set = (value: NonNullable<T>): void => {
+		currentValue = value;
+	};
 
-  return {
-    get,
-    set,
-  };
+	return {
+		get,
+		set,
+	};
 };
 
 type Container<T> = ReturnType<typeof buildContainer<T>>;
 
 const hasImport = (
-  importDeclarations: ImportDeclaration[],
-  moduleSpecifierText: string,
-  namedImportText: string
+	importDeclarations: ImportDeclaration[],
+	moduleSpecifierText: string,
+	namedImportText: string,
 ): boolean => {
-  return importDeclarations.some((importDeclaration) => {
-    const moduleSpecifier = importDeclaration.getModuleSpecifier();
+	return importDeclarations.some((importDeclaration) => {
+		const moduleSpecifier = importDeclaration.getModuleSpecifier();
 
-    if (moduleSpecifier.getLiteralText() !== moduleSpecifierText) {
-      return false;
-    }
+		if (moduleSpecifier.getLiteralText() !== moduleSpecifierText) {
+			return false;
+		}
 
-    return importDeclaration
-      .getNamedImports()
-      .some((namedImport) => namedImport.getName() === namedImportText);
-  });
+		return importDeclaration
+			.getNamedImports()
+			.some((namedImport) => namedImport.getName() === namedImportText);
+	});
 };
 
 const handlePAE = (
-  node: PropertyAccessExpression,
-  onReplacedWithSearchParams: () => void,
-  onReplacedWithPathname: () => void
+	node: PropertyAccessExpression,
+	onReplacedWithSearchParams: () => void,
+	onReplacedWithPathname: () => void,
 ) => {
-  // e.g. router.query
+	// e.g. router.query
 
-  const nodeName = node.getName();
+	const nodeName = node.getName();
 
-  if (nodeName === "query") {
-    const parentNode = node.getParent();
+	if (nodeName === 'query') {
+		const parentNode = node.getParent();
 
-    if (Node.isPropertyAccessExpression(parentNode)) {
-      // e.g. router.query.a
-      const parentNodeName = parentNode.getName();
+		if (Node.isPropertyAccessExpression(parentNode)) {
+			// e.g. router.query.a
+			const parentNodeName = parentNode.getName();
 
-      parentNode.replaceWithText(`searchParams.get("${parentNodeName}")`);
+			parentNode.replaceWithText(`searchParams.get("${parentNodeName}")`);
 
-      onReplacedWithSearchParams();
-      return;
-    }
+			onReplacedWithSearchParams();
+			return;
+		}
 
-    if (Node.isSpreadAssignment(parentNode)) {
-      parentNode.replaceWithText(`...Object.fromEntries(searchParams)`);
+		if (Node.isSpreadAssignment(parentNode)) {
+			parentNode.replaceWithText(`...Object.fromEntries(searchParams)`);
 
-      onReplacedWithSearchParams();
-      return;
-    }
+			onReplacedWithSearchParams();
+			return;
+		}
 
-    if (Node.isVariableDeclaration(parentNode)) {
-      const bindingName = parentNode.getNameNode();
+		if (Node.isVariableDeclaration(parentNode)) {
+			const bindingName = parentNode.getNameNode();
 
-      if (Node.isObjectBindingPattern(bindingName)) {
-        const bindingElements = bindingName.getElements();
+			if (Node.isObjectBindingPattern(bindingName)) {
+				const bindingElements = bindingName.getElements();
 
-        const names = bindingElements.map((bindingElement) => {
-          return bindingElement.getName();
-        });
+				const names = bindingElements.map((bindingElement) => {
+					return bindingElement.getName();
+				});
 
-        const vdl = parentNode.getFirstAncestorByKind(
-          ts.SyntaxKind.VariableDeclarationList
-        );
+				const vdl = parentNode.getFirstAncestorByKind(
+					ts.SyntaxKind.VariableDeclarationList,
+				);
 
-        for (const name of names) {
-          vdl?.addDeclaration({
-            name,
-            initializer: `searchParams.get("${name}")`,
-          });
-        }
+				for (const name of names) {
+					vdl?.addDeclaration({
+						name,
+						initializer: `searchParams.get("${name}")`,
+					});
+				}
 
-        parentNode.remove();
+				parentNode.remove();
 
-        onReplacedWithSearchParams();
-        return;
-      }
-    }
+				onReplacedWithSearchParams();
+				return;
+			}
+		}
 
-    node.replaceWithText("searchParams");
-    onReplacedWithSearchParams();
-  }
+		node.replaceWithText('searchParams');
+		onReplacedWithSearchParams();
+	}
 
-  if (nodeName === "pathname") {
-    node.replaceWithText("pathname");
-    onReplacedWithPathname();
-  }
+	if (nodeName === 'pathname') {
+		node.replaceWithText('pathname');
+		onReplacedWithPathname();
+	}
+};
+
+const handleQueryNode = (
+	node: Node,
+	onReplacedWithSearchParams: () => void,
+) => {
+	if (Node.isIdentifier(node)) {
+		const parent = node.getParent();
+
+		if (Node.isPropertyAccessExpression(parent)) {
+			const name = parent.getName();
+
+			parent.replaceWithText(`searchParams.get('${name}')`);
+
+			onReplacedWithSearchParams();
+		}
+	}
 };
 
 const handleReferencedNode = (
-  node: Node<ts.Node>,
-  usesSearchParams: Container<boolean>,
-  usesPathname: Container<boolean>
+	node: Node<ts.Node>,
+	usesSearchParams: Container<boolean>,
+	usesPathname: Container<boolean>,
 ) => {
-  if (Node.isIdentifier(node)) {
-    const block = node.getFirstAncestorByKind(ts.SyntaxKind.Block);
+	if (Node.isIdentifier(node)) {
+		const block = node.getFirstAncestorByKind(ts.SyntaxKind.Block);
 
-    const requiresSearchParams = buildContainer<boolean>(false); // TODO check if the statement exists
-    const requiresPathname = buildContainer<boolean>(false); // TODO check if the statement exists
+		const requiresSearchParams = buildContainer<boolean>(false); // TODO check if the statement exists
+		const requiresPathname = buildContainer<boolean>(false); // TODO check if the statement exists
 
-    const parent = node.getParent();
+		const parent = node.getParent();
 
-    if (Node.isCallExpression(parent)) {
-      // useRouter();
+		if (Node.isCallExpression(parent)) {
+			// useRouter();
 
-      const grandparent = parent.getParent();
+			const grandparent = parent.getParent();
 
-      if (Node.isVariableDeclaration(grandparent)) {
-        const bindingName = grandparent.getNameNode();
+			if (Node.isVariableDeclaration(grandparent)) {
+				const bindingName = grandparent.getNameNode();
 
-        if (Node.isIdentifier(bindingName)) {
-          // e.g. router
+				if (Node.isIdentifier(bindingName)) {
+					// e.g. router
 
-          bindingName.findReferencesAsNodes().forEach((node) => {
-            const parent = node.getParent();
+					bindingName.findReferencesAsNodes().forEach((node) => {
+						const parent = node.getParent();
 
-            if (Node.isPropertyAccessExpression(parent)) {
-              handlePAE(
-                parent,
-                () => requiresSearchParams.set(true),
-                () => requiresPathname.set(true)
-              );
-            }
-          });
-        }
-      }
-    }
+						if (Node.isPropertyAccessExpression(parent)) {
+							handlePAE(
+								parent,
+								() => requiresSearchParams.set(true),
+								() => requiresPathname.set(true),
+							);
+						}
+					});
+				}
 
-    if (requiresSearchParams.get()) {
-      block?.insertStatements(0, "const searchParams = useSearchParams();");
+				if (Node.isObjectBindingPattern(bindingName)) {
+					for (const element of bindingName.getElements()) {
+						const nameNode = element.getNameNode();
 
-      usesSearchParams.set(true);
-    }
+						if (Node.isIdentifier(nameNode)) {
+							if (nameNode.getText() === 'query') {
+								// find query.a usages
 
-    if (requiresPathname.get()) {
-      block?.insertStatements(0, "const pathname = usePathname();");
+								nameNode
+									.findReferencesAsNodes()
+									.forEach((node) => {
+										handleQueryNode(node, () =>
+											requiresSearchParams.set(true),
+										);
+									});
+							}
 
-      usesPathname.set(true);
-    }
-  }
+							// TODO ensure we remove it when all occurences have been replaced
+							grandparent.remove();
+						}
+					}
+				}
+			}
+		}
+
+		if (requiresSearchParams.get()) {
+			block?.insertStatements(
+				0,
+				'const searchParams = useSearchParams();',
+			);
+
+			usesSearchParams.set(true);
+		}
+
+		if (requiresPathname.get()) {
+			block?.insertStatements(0, 'const pathname = usePathname();');
+
+			usesPathname.set(true);
+		}
+	}
 };
 
 const handleImportDeclaration = (
-  importDeclaration: ImportDeclaration,
-  usesSearchParams: Container<boolean>,
-  usesPathname: Container<boolean>
+	importDeclaration: ImportDeclaration,
+	usesSearchParams: Container<boolean>,
+	usesPathname: Container<boolean>,
 ) => {
-  const moduleSpecifier = importDeclaration.getModuleSpecifier();
+	const moduleSpecifier = importDeclaration.getModuleSpecifier();
 
-  if (moduleSpecifier.getLiteralText() !== "next/router") {
-    return;
-  }
+	if (moduleSpecifier.getLiteralText() !== 'next/router') {
+		return;
+	}
 
-  importDeclaration.getNamedImports().forEach((namedImport) => {
-    if (namedImport.getName() !== "useRouter") {
-      return;
-    }
+	importDeclaration.getNamedImports().forEach((namedImport) => {
+		if (namedImport.getName() !== 'useRouter') {
+			return;
+		}
 
-    namedImport
-      .getNameNode()
-      .findReferencesAsNodes()
-      .forEach((node) =>
-        handleReferencedNode(node, usesSearchParams, usesPathname)
-      );
-  });
+		namedImport
+			.getNameNode()
+			.findReferencesAsNodes()
+			.forEach((node) =>
+				handleReferencedNode(node, usesSearchParams, usesPathname),
+			);
+	});
 
-  importDeclaration.remove();
+	importDeclaration.remove();
 };
 
 export const handleSourceFile = (
-  sourceFile: SourceFile
+	sourceFile: SourceFile,
 ): string | undefined => {
-  const usesSearchParams = buildContainer<boolean>(false);
-  const usesPathname = buildContainer<boolean>(false);
+	const usesSearchParams = buildContainer<boolean>(false);
+	const usesPathname = buildContainer<boolean>(false);
 
-  const importDeclarations = sourceFile.getImportDeclarations();
+	const importDeclarations = sourceFile.getImportDeclarations();
 
-  const hasUseRouterImport = hasImport(
-    importDeclarations,
-    "next/router",
-    "useRouter"
-  );
+	const hasUseRouterImport = hasImport(
+		importDeclarations,
+		'next/router',
+		'useRouter',
+	);
 
-  if (!hasUseRouterImport) {
-    return undefined;
-  }
+	if (!hasUseRouterImport) {
+		return undefined;
+	}
 
-  const hasUseSearchParamsImport = hasImport(
-    importDeclarations,
-    "next/navigation",
-    "useSearchParams"
-  );
+	const hasUseSearchParamsImport = hasImport(
+		importDeclarations,
+		'next/navigation',
+		'useSearchParams',
+	);
 
-  const hasUsePathnameImport = hasImport(
-    importDeclarations,
-    "next/navigation",
-    "usePathname"
-  );
+	const hasUsePathnameImport = hasImport(
+		importDeclarations,
+		'next/navigation',
+		'usePathname',
+	);
 
-  importDeclarations.forEach((importDeclaration) =>
-    handleImportDeclaration(importDeclaration, usesSearchParams, usesPathname)
-  );
+	importDeclarations.forEach((importDeclaration) =>
+		handleImportDeclaration(
+			importDeclaration,
+			usesSearchParams,
+			usesPathname,
+		),
+	);
 
-  sourceFile.insertStatements(
-    0,
-    'import { useRouter } from "next/navigation";'
-  );
+	sourceFile.insertStatements(
+		0,
+		'import { useRouter } from "next/navigation";',
+	);
 
-  if (usesSearchParams.get() && !hasUseSearchParamsImport) {
-    sourceFile.insertStatements(
-      0,
-      'import { useSearchParams } from "next/navigation";'
-    );
-  }
+	if (usesSearchParams.get() && !hasUseSearchParamsImport) {
+		sourceFile.insertStatements(
+			0,
+			'import { useSearchParams } from "next/navigation";',
+		);
+	}
 
-  if (usesPathname.get() && !hasUsePathnameImport) {
-    sourceFile.insertStatements(
-      0,
-      'import { usePathname } from "next/navigation";'
-    );
-  }
+	if (usesPathname.get() && !hasUsePathnameImport) {
+		sourceFile.insertStatements(
+			0,
+			'import { usePathname } from "next/navigation";',
+		);
+	}
 
-  return sourceFile.print({ emitHint: EmitHint.SourceFile });
+	return sourceFile.print({ emitHint: EmitHint.SourceFile });
 };
