2126b2f308ad7cc74cca232ddb0b2c84d1ae8604
Greg Pabian <35925521+grzpab@users.noreply.github.com>
2023-05-18 12:17:25 +0200

    wip

--- codemods/repomod-engine/next/13/app-directory-boilerplate/index.ts
+++ codemods/repomod-engine/next/13/app-directory-boilerplate/index.ts
@@ -1,5 +1,5 @@
-import { posix } from "node:path";
-import type { Repomod } from "@intuita-inc/repomod-engine-api";
+import { posix } from 'node:path';
+import type { Repomod } from '@intuita-inc/repomod-engine-api';
 
 type Dependencies = Readonly<{}>;
 
@@ -62,131 +62,131 @@ export default function Error({
 `;
 
 enum FilePurpose {
-  // root directory
-  ROOT_LAYOUT = "ROOT_LAYOUT",
-  ROOT_ERROR = "ROOT_ERROR",
-  ROOT_PAGE = "ROOT_PAGE",
-  ROOT_NOT_FOUND = "ROOT_NOT_FOUND",
-  // route directories
-  ROUTE_LAYOUT = "ROUTE_LAYOUT",
+	// root directory
+	ROOT_LAYOUT = 'ROOT_LAYOUT',
+	ROOT_ERROR = 'ROOT_ERROR',
+	ROOT_PAGE = 'ROOT_PAGE',
+	ROOT_NOT_FOUND = 'ROOT_NOT_FOUND',
+	// route directories
+	ROUTE_LAYOUT = 'ROUTE_LAYOUT',
 }
 
 export const repomod: Repomod<Dependencies> = {
-  includePatterns: ["**/pages/**/*.{js,jsx,ts,tsx}"],
-  excludePatterns: ["**/node_modules/**", "**/pages/api/**"],
-  handleFile: async (api, path, options) => {
-    const parsedPath = posix.parse(path);
-    const directoryNames = parsedPath.dir.split(posix.sep);
-    const endsWithPages =
-      directoryNames.length > 0 &&
-      directoryNames.lastIndexOf("pages") === directoryNames.length - 1;
+	includePatterns: ['**/pages/**/*.{js,jsx,ts,tsx}'],
+	excludePatterns: ['**/node_modules/**', '**/pages/api/**'],
+	handleFile: async (api, path, options) => {
+		const parsedPath = posix.parse(path);
+		const directoryNames = parsedPath.dir.split(posix.sep);
+		const endsWithPages =
+			directoryNames.length > 0 &&
+			directoryNames.lastIndexOf('pages') === directoryNames.length - 1;
 
-    const nameIsIndex = parsedPath.name === "index";
+		const nameIsIndex = parsedPath.name === 'index';
 
-    console.log(
-      directoryNames,
-      directoryNames.lastIndexOf("pages"),
-      directoryNames.length,
-      endsWithPages,
-      nameIsIndex
-    );
+		console.log(
+			directoryNames,
+			directoryNames.lastIndexOf('pages'),
+			directoryNames.length,
+			endsWithPages,
+			nameIsIndex,
+		);
 
-    if (endsWithPages && nameIsIndex) {
-      const rootLayoutPath = posix.format({
-        root: parsedPath.root,
-        dir: directoryNames.join(posix.sep),
-        ext: "tsx",
-        name: "layout",
-      });
+		if (endsWithPages && nameIsIndex) {
+			const rootLayoutPath = posix.format({
+				root: parsedPath.root,
+				dir: directoryNames.join(posix.sep),
+				ext: 'tsx',
+				name: 'layout',
+			});
 
-      const rootErrorPath = posix.format({
-        root: parsedPath.root,
-        dir: directoryNames.join(posix.sep),
-        ext: "tsx",
-        name: "error",
-      });
+			const rootErrorPath = posix.format({
+				root: parsedPath.root,
+				dir: directoryNames.join(posix.sep),
+				ext: 'tsx',
+				name: 'error',
+			});
 
-      const rootNotFoundPath = posix.format({
-        root: parsedPath.root,
-        dir: directoryNames.join(posix.sep),
-        ext: "tsx",
-        name: "not-found",
-      });
+			const rootNotFoundPath = posix.format({
+				root: parsedPath.root,
+				dir: directoryNames.join(posix.sep),
+				ext: 'tsx',
+				name: 'not-found',
+			});
 
-      const rootPagePath = posix.format({
-        root: parsedPath.root,
-        dir: directoryNames.join(posix.sep),
-        ext: "tsx",
-        name: "page",
-      });
+			const rootPagePath = posix.format({
+				root: parsedPath.root,
+				dir: directoryNames.join(posix.sep),
+				ext: 'tsx',
+				name: 'page',
+			});
 
-      return [
-        {
-          kind: "upsertFile",
-          path: rootLayoutPath,
-          options: {
-            ...options,
-            filePurpose: FilePurpose.ROOT_LAYOUT,
-          },
-        },
-        {
-          kind: "upsertFile",
-          path: rootErrorPath,
-          options: {
-            ...options,
-            filePurpose: FilePurpose.ROOT_ERROR,
-          },
-        },
-        {
-          kind: "upsertFile",
-          path: rootNotFoundPath,
-          options: {
-            ...options,
-            filePurpose: FilePurpose.ROOT_NOT_FOUND,
-          },
-        },
-        {
-          kind: "upsertFile",
-          path: rootPagePath,
-          options: {
-            ...options,
-            filePurpose: FilePurpose.ROOT_PAGE,
-          },
-        },
-      ];
-    }
+			return [
+				{
+					kind: 'upsertFile',
+					path: rootLayoutPath,
+					options: {
+						...options,
+						filePurpose: FilePurpose.ROOT_LAYOUT,
+					},
+				},
+				{
+					kind: 'upsertFile',
+					path: rootErrorPath,
+					options: {
+						...options,
+						filePurpose: FilePurpose.ROOT_ERROR,
+					},
+				},
+				{
+					kind: 'upsertFile',
+					path: rootNotFoundPath,
+					options: {
+						...options,
+						filePurpose: FilePurpose.ROOT_NOT_FOUND,
+					},
+				},
+				{
+					kind: 'upsertFile',
+					path: rootPagePath,
+					options: {
+						...options,
+						filePurpose: FilePurpose.ROOT_PAGE,
+					},
+				},
+			];
+		}
 
-    return [];
-  },
-  handleData: async (api, path, data, options) => {
-    const filePurpose = options.filePurpose;
+		return [];
+	},
+	handleData: async (api, path, data, options) => {
+		const filePurpose = options.filePurpose;
 
-    if (filePurpose === FilePurpose.ROOT_LAYOUT) {
-      return {
-        kind: "upsertData",
-        path,
-        data: ROOT_LAYOUT_CONTENT,
-      };
-    }
+		if (filePurpose === FilePurpose.ROOT_LAYOUT) {
+			return {
+				kind: 'upsertData',
+				path,
+				data: ROOT_LAYOUT_CONTENT,
+			};
+		}
 
-    if (filePurpose === FilePurpose.ROUTE_LAYOUT) {
-      return {
-        kind: "upsertData",
-        path,
-        data: ROUTE_LAYOUT_CONTENT,
-      };
-    }
+		if (filePurpose === FilePurpose.ROUTE_LAYOUT) {
+			return {
+				kind: 'upsertData',
+				path,
+				data: ROUTE_LAYOUT_CONTENT,
+			};
+		}
 
-    if (filePurpose === FilePurpose.ROOT_ERROR) {
-      return {
-        kind: "upsertData",
-        path,
-        data: ROOT_ERROR_CONTENT,
-      };
-    }
+		if (filePurpose === FilePurpose.ROOT_ERROR) {
+			return {
+				kind: 'upsertData',
+				path,
+				data: ROOT_ERROR_CONTENT,
+			};
+		}
 
-    return {
-      kind: "noop",
-    };
-  },
+		return {
+			kind: 'noop',
+		};
+	},
 };
--- codemods/repomod-engine/next/13/app-directory-boilerplate/test.ts
+++ codemods/repomod-engine/next/13/app-directory-boilerplate/test.ts
@@ -1,42 +1,42 @@
-import { Context } from "mocha";
+import { Context } from 'mocha';
 // import { deepStrictEqual } from "node:assert";
-import { Volume } from "memfs";
+import { Volume } from 'memfs';
 import {
-  FileSystemManager,
-  UnifiedFileSystem,
-  buildApi,
-  executeRepomod,
-} from "@intuita-inc/repomod-engine-api";
-import { repomod } from "./index.js";
+	FileSystemManager,
+	UnifiedFileSystem,
+	buildApi,
+	executeRepomod,
+} from '@intuita-inc/repomod-engine-api';
+import { repomod } from './index.js';
 
 const transform = async () => {
-  `import glob from 'glob';
+	`import glob from 'glob';
 import { promisify } from 'node:util';
 const promisifiedGlob = promisify(glob);`;
 
-  const volume = Volume.fromJSON({
-    "/opt/project/pages/index.js": "",
-  });
+	const volume = Volume.fromJSON({
+		'/opt/project/pages/index.js': '',
+	});
 
-  const fileSystemManager = new FileSystemManager(
-    volume.promises.readdir as any,
-    volume.promises.readFile as any,
-    volume.promises.stat as any
-  );
-  const unifiedFileSystem = new UnifiedFileSystem(
-    volume as any,
-    fileSystemManager
-  );
+	const fileSystemManager = new FileSystemManager(
+		volume.promises.readdir as any,
+		volume.promises.readFile as any,
+		volume.promises.stat as any,
+	);
+	const unifiedFileSystem = new UnifiedFileSystem(
+		volume as any,
+		fileSystemManager,
+	);
 
-  const api = buildApi<{}>(unifiedFileSystem, () => ({}));
+	const api = buildApi<{}>(unifiedFileSystem, () => ({}));
 
-  return executeRepomod(api, repomod, "/", {});
+	return executeRepomod(api, repomod, '/', {});
 };
 
-describe.only("next 13 replace-next-router", function () {
-  it("", async function (this: Context) {
-    const externalFileCommands = await transform();
+describe.only('next 13 replace-next-router', function () {
+	it('', async function (this: Context) {
+		const externalFileCommands = await transform();
 
-    console.log(externalFileCommands);
-  });
+		console.log(externalFileCommands);
+	});
 });
