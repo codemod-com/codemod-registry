ebb7cd0dfa682b78558fcc309ef125b00ac59b35
Greg Pabian <35925521+grzpab@users.noreply.github.com>
2023-05-30 13:01:37 +0200

    INT-1128 create a build script for S3 (#72)

---
+++ build.ts
@@ -0,0 +1,55 @@
+import { join } from "path";
+import {
+  existsSync,
+  mkdirSync,
+  readFileSync,
+  readdirSync,
+  writeFileSync,
+} from "fs";
+import { createHash } from "crypto";
+
+type Entry = Readonly<{
+  kind: "README";
+  name: string;
+  path: string;
+}>;
+const entries: Entry[] = [];
+
+const buildDirectoryPath = join(__dirname, "./build");
+
+mkdirSync(buildDirectoryPath, { recursive: true });
+
+const rootDirectory = join(__dirname, "./codemods/jscodeshift/next/13");
+
+const directories = readdirSync(rootDirectory);
+
+for (const directory of directories) {
+  const configPath = join(rootDirectory, directory, "config.json");
+  const readmePath = join(rootDirectory, directory, "README.md");
+
+  if (!existsSync(readmePath)) {
+    continue;
+  }
+
+  const config = readFileSync(configPath, "utf8");
+  const readme = readFileSync(readmePath, "utf8");
+
+  const jsonConfig = JSON.parse(config);
+
+  const hashDigest = createHash("ripemd160")
+    .update("README.md")
+    .update(jsonConfig.name)
+    .digest("base64url");
+
+  const newReadmePath = join(buildDirectoryPath, `${hashDigest}.md`);
+
+  entries.push({
+    kind: "README",
+    name: jsonConfig.name,
+    path: `${hashDigest}.md`,
+  });
+
+  writeFileSync(newReadmePath, readme);
+}
+
+writeFileSync(join(buildDirectoryPath, "index.json"), JSON.stringify(entries));
