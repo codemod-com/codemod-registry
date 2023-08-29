ea19c131f5a55497b52b9c1aabcae96e028f54a0
Benny Joo <sldisek783@gmail.com>
2023-08-29 13:16:06 +0100

    ember: jquery-apis

---
+++ ember/5/jquery-apis/index.js
@@ -0,0 +1,86 @@
+import codemodCLI from 'codemod-cli';
+
+const {
+	jscodeshift: { getParser },
+} = codemodCLI;
+
+export default function transformer(file, api) {
+	const j = getParser(api);
+
+	const root = j(file.source);
+
+	root.find(j.CallExpression, {
+		callee: {
+			object: {
+				callee: {
+					object: {
+						type: 'ThisExpression',
+					},
+					property: {
+						name: '$',
+					},
+				},
+			},
+		},
+	})
+		.forEach(() => {
+			//console.log(path);
+		})
+		.replaceWith((path) => {
+			//callee.object.arguments
+			//console.log(path.value.callee.object.arguments);
+			const isQuerySelector =
+				path.value.callee.object.arguments.length > 0;
+			//console.log(isQuerySelector);
+			let newCallExp;
+			if (isQuerySelector) {
+				newCallExp = j.callExpression(
+					j.memberExpression(
+						j.callExpression(
+							j.memberExpression(
+								j.memberExpression(
+									j.thisExpression(),
+									j.identifier('element'),
+									false,
+								),
+								j.identifier('querySelectorAll'),
+							),
+							path.value.callee.object.arguments,
+						),
+						j.identifier('forEach'),
+						false,
+					),
+					[
+						j.arrowFunctionExpression(
+							[j.identifier('el')],
+							j.callExpression(
+								j.memberExpression(
+									j.identifier('el'),
+									j.identifier('addEventListener'),
+									false,
+								),
+								path.value.arguments,
+							),
+							false,
+						),
+					],
+				);
+			} else {
+				newCallExp = j.callExpression(
+					j.memberExpression(
+						j.memberExpression(
+							j.thisExpression(),
+							j.identifier('element'),
+							false,
+						),
+						j.identifier('addEventListener'),
+						false,
+					),
+					path.value.arguments,
+				);
+			}
+			return newCallExp;
+		});
+
+	return root.toSource();
+}
