533c43a5339c33200836e677a84243a75952781e
Benny Joo <sldisek783@gmail.com>
2023-08-29 11:50:05 +0100

    ember: array-wrapper

---
+++ ember/5/array-wrapper/index.js
@@ -0,0 +1,30 @@
+import codemodCLI from 'codemod-cli';
+
+const {
+	jscodeshift: { getParser },
+} = codemodCLI;
+
+export default function transformer(file, api) {
+	const j = getParser(api);
+
+	const root = j(file.source);
+
+	root.find(j.NewExpression, {
+		callee: {
+			name: 'A',
+		},
+	})
+	.replaceWith(() => {
+		root.find(j.ImportSpecifier, {
+			imported: { name: 'A' },
+			local: { name: 'A' },
+		})
+		.replaceWith(() => {
+			return j.importSpecifier(j.identifier('A'), j.identifier('emberA'));
+		});
+
+		return j.callExpression(j.identifier('A'), []);
+	});
+
+	return root.toSource();
+}
