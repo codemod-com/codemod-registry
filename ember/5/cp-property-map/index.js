34b1ddeba38afa55ca308fd8a06f1037b30c48bd
Benny Joo <sldisek783@gmail.com>
2023-08-29 11:53:11 +0100

    prettier

--- ember/5/cp-property-map/index.js
+++ ember/5/cp-property-map/index.js
@@ -15,8 +15,7 @@ export default function transformer(file, api) {
 			object: { callee: { name: 'map' } },
 			property: { name: 'property' },
 		},
-	})
-	.replaceWith((path) => {
+	}).replaceWith((path) => {
 		let calleeArgs = path.value.callee.object.arguments;
 		let first = calleeArgs.slice(0, calleeArgs.length - 1);
 		let last = calleeArgs[calleeArgs.length - 1];
