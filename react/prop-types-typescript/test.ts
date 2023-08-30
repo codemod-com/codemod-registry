6f62ec09a7b0d080949d19f835c21f91e3988460
dhryshyn <dev.dmytroh@gmail.com>
2023-08-30 12:20:49 +0300

    fix

--- react/prop-types-typescript/test.ts
+++ react/prop-types-typescript/test.ts
@@ -1,9 +1,31 @@
+/* @license
+
+ISC License
+
+Copyright (c) 2023, Mark Skelton
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted, provided that the above
+copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+
+/*
+Changes to the original file: changed tests structure
+*/
+
 import assert from 'node:assert';
 import transform from './index.js';
 import { buildApi } from '../../utilities.js';
 
-// auto imported from https://github.com/mskelton/ratchet/tree/main/__testfixtures__
-describe.only('ratchet', function () {
+describe('ratchet', function () {
 	it('arrow-function', function () {
 		const INPUT =
 			'import PropTypes from "prop-types"\nimport React from "react"\n\nexport const MyComponent = (props) => {\n  return <span />\n}\n\nMyComponent.propTypes = {\n  bar: PropTypes.string.isRequired,\n  foo: PropTypes.number,\n}\n';
