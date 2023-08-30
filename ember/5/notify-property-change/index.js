985462a96cde9ff31095310df7c5023748638228
Benny Joo <sldisek783@gmail.com>
2023-08-30 11:58:43 +0100

    remove codemod-cli library and remove its usage

--- ember/5/notify-property-change/index.js
+++ ember/5/notify-property-change/index.js
@@ -27,14 +27,8 @@
  * License URL: https://github.com/ember-codemods/ember-no-implicit-this-codemod/blob/master/LICENSE
  */
 
-import codemodCLI from 'codemod-cli';
-
-const {
-	jscodeshift: { getParser },
-} = codemodCLI;
-
 export default function transformer(file, api) {
-	const j = getParser(api);
+	const j = api.jscodeshift;
 	const root = j(file.source);
 
 	root.find(j.ExpressionStatement, {
