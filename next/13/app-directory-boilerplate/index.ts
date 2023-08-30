ca489d5bdf998ba25685fc44390a5f1066076e95
Greg Pabian <35925521+grzpab@users.noreply.github.com>
2023-08-30 11:27:44 +0200

    INT-1550 add helper functions for the boilerplate codemod (from another stale PR) (#195)

--- next/13/app-directory-boilerplate/index.ts
+++ next/13/app-directory-boilerplate/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { join, posix } from 'node:path';
 import tsmorph, {
 	ArrowFunction,
@@ -78,12 +79,380 @@ const map = new Map([
 const EXTENSION = '.tsx';
 
 type FileAPI = Parameters<NonNullable<Repomod<Dependencies>['handleFile']>>[0];
+type DataAPI = Parameters<NonNullable<Repomod<Dependencies>['handleData']>>[0];
 
-type Unpromisify<T> = T extends Promise<infer U> ? U : never;
-
-type FileCommand = Unpromisify<
+type FileCommand = Awaited<
 	ReturnType<NonNullable<Repomod<Dependencies>['handleFile']>>
 >[number];
+
+type DataCommand = Awaited<
+	ReturnType<NonNullable<Repomod<Dependencies>['handleData']>>
+>;
+
+const removeUnneededImportDeclarations = (sourceFile: SourceFile) => {
+	sourceFile
+		.getImportDeclarations()
+		.filter((declaration) => {
+			if (
+				declaration.getModuleSpecifier().getLiteralText() ===
+					'next/head' &&
+				declaration.getImportClause()?.getText() === 'Head'
+			) {
+				return true;
+			}
+
+			const defaultImport = declaration.getDefaultImport() ?? null;
+
+			if (defaultImport !== null) {
+				const text = defaultImport.getText();
+
+				return (
+					sourceFile
+						.getDescendantsOfKind(tsmorph.ts.SyntaxKind.Identifier)
+						.filter((identifier) => identifier.getText() === text)
+						.length === 1
+				);
+			}
+
+			return declaration.getNamedImports().every((namedImport) => {
+				const text = namedImport.getText();
+
+				return (
+					sourceFile
+						.getDescendantsOfKind(tsmorph.ts.SyntaxKind.Identifier)
+						.filter((identifier) => identifier.getText() === text)
+						.length === 1
+				);
+			});
+		})
+		.forEach((declaration) => {
+			declaration.remove();
+		});
+};
+
+const buildComponentsFileData = (
+	api: DataAPI,
+	path: string,
+	options: Readonly<Record<string, string | undefined>>,
+	filePurpose: FilePurpose.ROOT_COMPONENTS | FilePurpose.ROUTE_COMPONENTS,
+): DataCommand => {
+	const { tsmorph, parseMdx, stringifyMdx, visitMdxAst } =
+		api.getDependencies();
+
+	let sourcingStatementInserted = false;
+
+	const rewriteWithTsMorph = (input: string) => {
+		const project = new tsmorph.Project({
+			useInMemoryFileSystem: true,
+			skipFileDependencyResolution: true,
+			compilerOptions: {
+				allowJs: true,
+			},
+		});
+
+		const sourceFile = project.createSourceFile(
+			options.oldPath ?? '',
+			input,
+		);
+
+		sourceFile.getFunctions().forEach((fn) => {
+			const id = fn.getName() ?? '';
+
+			if (
+				[
+					'getStaticProps',
+					'getServerSideProps',
+					'getStaticPaths',
+					'generateMetadata',
+				].includes(id)
+			) {
+				fn.remove();
+			}
+		});
+
+		sourceFile.getVariableStatements().forEach((statement) => {
+			statement.getDeclarations().forEach((declaration) => {
+				const id = declaration.getName() ?? '';
+
+				if (
+					[
+						'getStaticProps',
+						'getServerSideProps',
+						'getStaticPaths',
+						'generateMetadata',
+						'metadata',
+					].includes(id)
+				) {
+					declaration.remove();
+				}
+			});
+		});
+
+		removeUnneededImportDeclarations(sourceFile);
+
+		sourceFile
+			.getDescendantsOfKind(SyntaxKind.JsxOpeningElement)
+			.filter(
+				(jsxOpeningElement) =>
+					jsxOpeningElement.getTagNameNode().getText() === 'Head',
+			)
+			.map((declaration) => {
+				return declaration.getFirstAncestorByKind(
+					SyntaxKind.JsxElement,
+				);
+			})
+			.forEach((jsxElement) => {
+				const parenthesizedExpressionParent =
+					jsxElement?.getParentIfKind(
+						SyntaxKind.ParenthesizedExpression,
+					) ?? null;
+
+				if (parenthesizedExpressionParent !== null) {
+					parenthesizedExpressionParent.replaceWithText('null');
+
+					return;
+				}
+
+				jsxElement?.replaceWithText('');
+			});
+
+		if (filePurpose === FilePurpose.ROUTE_COMPONENTS) {
+			sourceFile.getImportDeclarations().forEach((declaration) => {
+				const moduleSpecifier = declaration.getModuleSpecifierValue();
+
+				if (moduleSpecifier.startsWith('./')) {
+					declaration.setModuleSpecifier(`.${moduleSpecifier}`);
+				} else if (moduleSpecifier.startsWith('../')) {
+					declaration.setModuleSpecifier(`../${moduleSpecifier}`);
+				}
+			});
+		}
+
+		if (!sourcingStatementInserted) {
+			sourceFile.insertStatements(0, [
+				`'use client';`,
+				`// This file has been sourced from: ${options.oldPath}`,
+			]);
+
+			sourcingStatementInserted = true;
+		}
+
+		return sourceFile.print();
+	};
+
+	if (path.endsWith('.mdx')) {
+		if (parseMdx && stringifyMdx && visitMdxAst) {
+			const tree = parseMdx(options.oldData ?? '');
+
+			visitMdxAst(tree, (node) => {
+				if (node.type === 'mdxjsEsm') {
+					node.value = rewriteWithTsMorph(node.value);
+
+					delete node.data;
+					delete node.position;
+
+					return 'skip';
+				}
+			});
+
+			const data = stringifyMdx(tree);
+
+			return {
+				kind: 'upsertData',
+				path,
+				data,
+			};
+		} else {
+			return {
+				kind: 'noop',
+			};
+		}
+	}
+
+	return {
+		kind: 'upsertData',
+		path,
+		data: rewriteWithTsMorph(options.oldData ?? ''),
+	};
+};
+
+const buildPageFileData = (
+	api: DataAPI,
+	path: string,
+	options: Readonly<Record<string, string | undefined>>,
+	filePurpose: FilePurpose.ROOT_PAGE | FilePurpose.ROUTE_PAGE,
+): DataCommand => {
+	const { tsmorph, parseMdx, stringifyMdx, visitMdxAst } =
+		api.getDependencies();
+
+	let sourcingStatementInserted = false;
+
+	const rewriteWithTsMorph = (input: string): string => {
+		const project = new tsmorph.Project({
+			useInMemoryFileSystem: true,
+			skipFileDependencyResolution: true,
+			compilerOptions: {
+				allowJs: true,
+			},
+		});
+
+		const sourceFile = project.createSourceFile(
+			options.oldPath ?? '',
+			input,
+		);
+
+		sourceFile.getFunctions().forEach((fn) => {
+			if (fn.isDefaultExport()) {
+				fn.remove();
+				return;
+			}
+
+			const id = fn.getName() ?? '';
+
+			if (
+				[
+					'getStaticProps',
+					'getServerSideProps',
+					'getStaticPaths',
+				].includes(id)
+			) {
+				fn.setIsExported(false);
+			}
+		});
+
+		sourceFile.getVariableStatements().forEach((statement) => {
+			statement.getDeclarations().forEach((declaration) => {
+				const id = declaration.getName() ?? '';
+
+				if (
+					[
+						'getStaticProps',
+						'getServerSideProps',
+						'getStaticPaths',
+					].includes(id) &&
+					declaration.hasExportKeyword()
+				) {
+					statement.setIsExported(false);
+				}
+			});
+		});
+
+		removeUnneededImportDeclarations(sourceFile);
+
+		sourceFile
+			.getDescendantsOfKind(SyntaxKind.JsxOpeningElement)
+			.filter(
+				(jsxOpeningElement) =>
+					jsxOpeningElement.getTagNameNode().getText() === 'Head',
+			)
+			.map((declaration) => {
+				return declaration.getFirstAncestorByKind(
+					SyntaxKind.JsxElement,
+				);
+			})
+			.forEach((jsxElement) => {
+				const parenthesizedExpressionParent =
+					jsxElement?.getParentIfKind(
+						SyntaxKind.ParenthesizedExpression,
+					) ?? null;
+
+				if (parenthesizedExpressionParent !== null) {
+					parenthesizedExpressionParent.replaceWithText('null');
+
+					return;
+				}
+
+				jsxElement?.replaceWithText('');
+			});
+
+		if (filePurpose === FilePurpose.ROUTE_PAGE) {
+			sourceFile.getImportDeclarations().forEach((declaration) => {
+				const moduleSpecifier = declaration.getModuleSpecifierValue();
+
+				if (moduleSpecifier.startsWith('./')) {
+					declaration.setModuleSpecifier(`.${moduleSpecifier}`);
+				} else if (moduleSpecifier.startsWith('../')) {
+					declaration.setModuleSpecifier(`../${moduleSpecifier}`);
+				}
+			});
+		}
+
+		sourceFile.addImportDeclaration({
+			moduleSpecifier: './components',
+			defaultImport: 'Components',
+		});
+
+		sourceFile.getStatementsWithComments().forEach((statement, index) => {
+			if (tsmorph.Node.isVariableStatement(statement)) {
+				const declarations = statement
+					.getDeclarationList()
+					.getDeclarations();
+
+				const getServerSidePropsUsed = declarations.some(
+					(declaration) =>
+						declaration.getName() === 'getServerSideProps',
+				);
+
+				if (getServerSidePropsUsed) {
+					sourceFile.insertStatements(
+						index,
+						'// TODO reimplement getServerSideProps with custom logic\n',
+					);
+				}
+			}
+		});
+
+		if (!sourcingStatementInserted) {
+			sourceFile.insertStatements(
+				0,
+				`// This file has been sourced from: ${options.oldPath}`,
+			);
+
+			sourcingStatementInserted = true;
+		}
+
+		sourceFile.addStatements(`export default async function Page(props: any) {
+			return <Components {...props} />;
+		}`);
+
+		return sourceFile.print();
+	};
+
+	if (path.endsWith('.mdx')) {
+		if (parseMdx && stringifyMdx && visitMdxAst) {
+			const tree = parseMdx(options.oldData ?? '');
+
+			visitMdxAst(tree, (node) => {
+				if (node.type === 'mdxjsEsm') {
+					node.value = rewriteWithTsMorph(node.value);
+
+					delete node.data;
+					delete node.position;
+
+					return 'skip';
+				}
+			});
+
+			const data = stringifyMdx(tree);
+
+			return {
+				kind: 'upsertData',
+				path,
+				data,
+			};
+		} else {
+			return {
+				kind: 'noop',
+			};
+		}
+	}
+
+	return {
+		kind: 'upsertData',
+		path,
+		data: rewriteWithTsMorph(options.oldData ?? ''),
+	};
+};
 
 const resolveExtensionlessFilePath = (
 	extensionlessFilePath: string,
--- next/13/remove-get-static-props/test.ts
+++ next/13/remove-get-static-props/test.ts
@@ -1050,7 +1050,7 @@ describe('next 13 remove-get-static-props', function () {
 		};
 
 		const actualOutput = transform(fileInfo, buildApi('tsx'));
-		console.log(actualOutput, '?');
+
 		assert.deepEqual(
 			actualOutput?.replace(/\W/gm, ''),
 			OUTPUT?.replace(/\W/gm, ''),
