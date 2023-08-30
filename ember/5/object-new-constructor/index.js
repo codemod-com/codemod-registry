3269a1390821f47f70a453a778ca3308ed7a6522
Benny Joo <sldisek783@gmail.com>
2023-08-30 11:58:43 +0100

    remove codemod-cli library and remove its usage

--- ember/5/object-new-constructor/index.js
+++ ember/5/object-new-constructor/index.js
@@ -6,14 +6,8 @@
  * License URL: https://opensource.org/licenses/MIT
  */
 
-import codemodCLI from 'codemod-cli';
-
-const {
-	jscodeshift: { getParser },
-} = codemodCLI;
-
 export default function transformer(file, api) {
-	const j = getParser(api);
+	const j = api.jscodeshift;
 	const root = j(file.source);
 
 	root.find(j.NewExpression, {
