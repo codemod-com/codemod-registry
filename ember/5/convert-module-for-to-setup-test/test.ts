9a91a2a00ae8fe517120d5a557a7b573d8e4a9ef
Benny Joo <sldisek783@gmail.com>
2023-09-04 11:39:26 +0100

    add license for test

--- ember/5/convert-module-for-to-setup-test/test.ts
+++ ember/5/convert-module-for-to-setup-test/test.ts
@@ -3,6 +3,35 @@ import assert from 'node:assert';
 import transform from './index.js';
 import { buildApi } from '../../../utilities.js';
 
+/**
+ * These test fixtures are based on public tests, which is subject to the original license terms.
+ * Original tests: https://github.com/ember-codemods/ember-qunit-codemod/tree/master/transforms/convert-module-for-to-setup-test/__testfixtures__
+ *
+ * License: 
+ 	MIT License
+
+	Copyright (c) 2019 ember-codemods
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+ * License URL: https://github.com/ember-codemods/ember-no-implicit-this-codemod/blob/master/LICENSE
+ */
+
 describe('ember 5 convert-module-for-to-setup-test', function () {
 	it('basic-typescript-support', function () {
 		const INPUT = `
