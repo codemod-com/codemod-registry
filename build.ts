7a5d7e2dde9ad0870c309e415df132065c46b2c4
Greg Pabian <35925521+grzpab@users.noreply.github.com>
2023-09-08 13:55:40 +0200

    INT-1823 allow the codemod creators to specify codemod arguments (#254)

--- build.ts
+++ build.ts
@@ -22,28 +22,57 @@ import * as tar from 'tar';
 
 const promisifiedDeflate = promisify(deflate);
 
+const argumentsSchema = S.array(
+	S.union(
+		S.struct({
+			name: S.string,
+			kind: S.literal('string'),
+			default: S.union(S.string, S.undefined),
+		}),
+		S.struct({
+			name: S.string,
+			kind: S.literal('number'),
+			default: S.union(S.number, S.undefined),
+		}),
+		S.struct({
+			name: S.string,
+			kind: S.literal('boolean'),
+			default: S.union(S.boolean, S.undefined),
+		}),
+	),
+);
+
+const optionalArgumentsSchema = S.optional(argumentsSchema).withDefault(
+	() => [],
+);
+
 const codemodConfigSchema = S.union(
 	S.struct({
 		schemaVersion: S.literal('1.0.0'),
 		engine: S.literal('piranha'),
 		language: S.literal('java'),
+		arguments: optionalArgumentsSchema,
 	}),
 	S.struct({
 		schemaVersion: S.literal('1.0.0'),
 		engine: S.literal('jscodeshift'),
+		arguments: optionalArgumentsSchema,
 	}),
 	S.struct({
 		schemaVersion: S.literal('1.0.0'),
 		engine: S.literal('ts-morph'),
+		arguments: optionalArgumentsSchema,
 	}),
 	S.struct({
 		schemaVersion: S.literal('1.0.0'),
 		engine: S.literal('repomod-engine'),
+		arguments: optionalArgumentsSchema,
 	}),
 	S.struct({
 		schemaVersion: S.literal('1.0.0'),
 		engine: S.literal('recipe'),
 		names: S.array(S.string),
+		arguments: optionalArgumentsSchema,
 	}),
 );
 
