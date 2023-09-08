39af8fb718e565d3f09a57c50c68743c46862cf5
Greg Pabian <35925521+grzpab@users.noreply.github.com>
2023-09-08 16:30:21 +0200

    INT-1824 a prototype codemod for feature flag removal (#255)

---
+++ remove-unused-feature-flags/test.ts
@@ -0,0 +1,63 @@
+import { FileInfo } from 'jscodeshift';
+import assert from 'node:assert';
+import transform from './index.js';
+import { buildApi } from '../utilities.js';
+
+describe('remove-unused-feature-flags', function () {
+	it('should remove a feature flag check within Promise.all()', function () {
+		const INPUT = `
+        const [a, b] = await Promise.all([
+            Promise.resolve('a'),
+            isFlagEnabled('featureFlag'),
+        ]);
+		`;
+
+		const OUTPUT = `
+        const [a] = await Promise.all([
+            Promise.resolve('a')
+        ]);
+        `;
+
+		const fileInfo: FileInfo = {
+			path: 'index.ts',
+			source: INPUT,
+		};
+
+		const actualOutput = transform(fileInfo, buildApi('ts'), {});
+
+		assert.deepEqual(
+			actualOutput?.replace(/\s/gm, ''),
+			OUTPUT.replace(/\s/gm, ''),
+		);
+	});
+
+	it('should remove a feature flag check within Promise.all() (with options)', function () {
+		const INPUT = `
+        const [a, b] = await Promise.all([
+            Promise.resolve('a'),
+            fnc('b'),
+        ]);
+		`;
+
+		const OUTPUT = `
+        const [a] = await Promise.all([
+            Promise.resolve('a')
+        ]);
+        `;
+
+		const fileInfo: FileInfo = {
+			path: 'index.ts',
+			source: INPUT,
+		};
+
+		const actualOutput = transform(fileInfo, buildApi('ts'), {
+			functionName: 'fnc',
+			featureFlagName: 'b',
+		});
+
+		assert.deepEqual(
+			actualOutput?.replace(/\s/gm, ''),
+			OUTPUT.replace(/\s/gm, ''),
+		);
+	});
+});
