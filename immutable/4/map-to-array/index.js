d0d0c71217ed860769ed7deb0f0ebc2e2f67a8fd
Benny Joo <sldisek783@gmail.com>
2023-09-12 10:57:37 +0100

    bug fix: transformer -> transform (#292)

--- immutable/4/map-to-array/index.js
+++ immutable/4/map-to-array/index.js
@@ -26,7 +26,7 @@ THE SOFTWARE.
 Changes to the original file: added options
 */
 
-export default function transformer(file, api, options) {
+export default function transform(file, api, options) {
 	const j = api.jscodeshift;
 
 	const root = j(file.source);
