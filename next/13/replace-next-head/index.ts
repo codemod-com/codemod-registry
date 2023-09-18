9a8ebe6f70d4b01bdd075aa8165dacd55edb8ccf
dhryshyn <dev.dmytroh@gmail.com>
2023-09-18 14:11:31 +0300

    fix dependency detector

--- next/13/replace-next-head/index.ts
+++ next/13/replace-next-head/index.ts
@@ -238,11 +238,28 @@ const getDependenciesForIdentifiers = (
 				continue;
 			}
 
-			ancestor =
-				firstDefinition.getKind() === syntaxKind
-					? firstDefinition
-					: firstDefinition.getFirstAncestorByKind(syntaxKind) ??
-					  null;
+			if (firstDefinition.getKind() === syntaxKind) {
+				ancestor = firstDefinition;
+			} else {
+				if (
+					syntaxKind === SyntaxKind.FunctionDeclaration &&
+					Node.isFunctionDeclaration(firstDefinition)
+				) {
+					ancestor = firstDefinition;
+				} else if (
+					syntaxKind === SyntaxKind.VariableStatement &&
+					Node.isVariableDeclaration(firstDefinition)
+				) {
+					ancestor = firstDefinition.getParent()?.getParent() ?? null;
+				} else if (
+					syntaxKind === SyntaxKind.Parameter ||
+					syntaxKind === SyntaxKind.ImportDeclaration
+				) {
+					ancestor =
+						firstDefinition.getFirstAncestorByKind(syntaxKind) ??
+						null;
+				}
+			}
 		}
 
 		if (ancestor === null) {
@@ -1716,7 +1733,7 @@ const getTsCompilerOptions = async (api: FileAPI, baseUrl: string) => {
 		const configWithoutComments = tsConfigStr.replace(/^\s*?\/\/.*$/gm, '');
 		return JSON.parse(configWithoutComments).compilerOptions;
 	} catch (e) {
-		console.error(e);
+		// console.error(e);
 		return {};
 	}
 };
@@ -1748,7 +1765,6 @@ export const repomod: Repomod<Dependencies, Record<string, unknown>> = {
 		}
 
 		const metadataTree = buildMetadataTreeNode(path, path);
-
 		if (Object.keys(metadataTree).length === 0) {
 			return [];
 		}
