3241af9f22864e026450ba9d7f8f1cba0c393c60
Mohab Sameh <mohab@intuita.io>
2023-09-22 17:33:49 +0300

    run prettier

--- netlify-sdk/0.8.4/addApiHandler/index.ts
+++ netlify-sdk/0.8.4/addApiHandler/index.ts
@@ -1,26 +1,26 @@
 import type { FileInfo, API, Options } from 'jscodeshift';
 export default function transform(
-    file: FileInfo,
-    api: API,
-    options: Options,
+	file: FileInfo,
+	api: API,
+	options: Options,
 ): string | undefined {
-    const j = api.jscodeshift;
-    const root = j(file.source);
+	const j = api.jscodeshift;
+	const root = j(file.source);
 
-    // Find all calls to addHandler method
-    root.find(j.CallExpression, {
-        callee: {
-            type: 'MemberExpression',
-            property: {
-                type: 'Identifier',
-                name: 'addHandler',
-            },
-        },
-    }).replaceWith((path) => {
-        // Replace addHandler with addApiHandler
-        path.node.callee.property.name = 'addApiHandler';
-        return path.node;
-    });
+	// Find all calls to addHandler method
+	root.find(j.CallExpression, {
+		callee: {
+			type: 'MemberExpression',
+			property: {
+				type: 'Identifier',
+				name: 'addHandler',
+			},
+		},
+	}).replaceWith((path) => {
+		// Replace addHandler with addApiHandler
+		path.node.callee.property.name = 'addApiHandler';
+		return path.node;
+	});
 
-    return root.toSource();
+	return root.toSource();
 }
