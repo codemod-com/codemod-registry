15b8104fadcc1d28923d397e0ea9b828b48cc4c9
Sasha <reactionnnnn@gmail.com>
2023-12-12 18:31:11 +0200

    feat: mocha -> vitest filemod (#523)

---
+++ codemods/mocha/vitest/config-files/src/index.ts
@@ -0,0 +1,154 @@
+import type { Filemod } from '@intuita-inc/filemod';
+import {
+	array,
+	is,
+	object,
+	optional,
+	record,
+	string,
+	type Input,
+} from 'valibot';
+
+const packageJsonSchema = object({
+	name: optional(string()),
+	dependencies: optional(record(string())),
+	devDependencies: optional(record(string())),
+	scripts: optional(record(string())),
+	mocha: optional(record(string())),
+});
+
+const tsconfigSchema = object({
+	compilerOptions: optional(object({ types: optional(array(string())) })),
+	include: optional(array(string())),
+});
+
+export const repomod: Filemod<Record<string, never>, Record<string, never>> = {
+	includePatterns: [
+		'**/package.json',
+		'**/tsconfig.json',
+		'**/{,.}{mocharc,mocha.config}{,.js,.json,.cjs,.mjs,.yaml,.yml}',
+	],
+	excludePatterns: ['**/node_modules/**'],
+	handleFile: async (_, path, options) => {
+		if (path.endsWith('tsconfig.json') || path.endsWith('package.json')) {
+			return [{ kind: 'upsertFile', path, options }];
+		}
+
+		if (path.includes('mocha')) {
+			return [{ kind: 'deleteFile', path }];
+		}
+
+		return [];
+	},
+	handleData: async (_, path, data) => {
+		if (path.endsWith('package.json')) {
+			let packageJson: Input<typeof packageJsonSchema>;
+			try {
+				const json = JSON.parse(data);
+				if (!is(packageJsonSchema, json)) {
+					return { kind: 'noop' };
+				}
+				packageJson = json;
+			} catch (err) {
+				return { kind: 'noop' };
+			}
+
+			// Remove possible "mocha" key and its value
+			if (packageJson.mocha) {
+				delete packageJson.mocha;
+			}
+
+			// Remove mocha from dependencies & devDependencies, add vitest devDep
+			if (packageJson.dependencies) {
+				Object.keys(packageJson.dependencies).forEach((dep) => {
+					if (dep.includes('mocha')) {
+						delete packageJson.dependencies![dep];
+					}
+				});
+			}
+			if (packageJson.devDependencies) {
+				Object.keys(packageJson.devDependencies).forEach((dep) => {
+					if (dep.includes('mocha')) {
+						delete packageJson.devDependencies![dep];
+					}
+				});
+			}
+			packageJson.devDependencies = {
+				...packageJson.devDependencies,
+				vitest: '^1.0.1',
+				'@vitest/coverage-v8': '^1.0.1',
+			};
+
+			// Remove commands using mocha
+			if (packageJson.scripts) {
+				Object.entries(packageJson.scripts).forEach(
+					([name, script]) => {
+						if (script.includes('mocha')) {
+							delete packageJson.scripts![name];
+						}
+					},
+				);
+			}
+
+			// Add vitest commands
+			if (packageJson.scripts) {
+				packageJson.scripts = {
+					...packageJson.scripts,
+					test: 'vitest run',
+					coverage: 'vitest run --coverage',
+				};
+			}
+
+			return {
+				kind: 'upsertData',
+				path,
+				data: JSON.stringify(packageJson, null, 2),
+			};
+		}
+
+		if (path.endsWith('tsconfig.json')) {
+			let tsconfigJson: Input<typeof tsconfigSchema>;
+			try {
+				const json = JSON.parse(data);
+				if (!is(tsconfigSchema, json)) {
+					return { kind: 'noop' };
+				}
+				tsconfigJson = json;
+			} catch (err) {
+				return { kind: 'noop' };
+			}
+
+			// Remove possible `types: ['mocha']`
+			if (tsconfigJson.compilerOptions?.types) {
+				const newTypes = tsconfigJson.compilerOptions.types.filter(
+					(type) => type !== 'mocha',
+				);
+
+				if (newTypes.length) {
+					tsconfigJson.compilerOptions.types = newTypes;
+				} else {
+					delete tsconfigJson.compilerOptions.types;
+				}
+			}
+			if (tsconfigJson.include) {
+				const newIncludes = tsconfigJson.include.filter(
+					(type) => type !== 'mocha',
+				);
+
+				if (newIncludes.length) {
+					tsconfigJson.include = newIncludes;
+				} else {
+					delete tsconfigJson.include;
+				}
+			}
+
+			return {
+				kind: 'upsertData',
+				path,
+				data: JSON.stringify(tsconfigJson, null, 2),
+			};
+		}
+
+		return { kind: 'noop' };
+	},
+};
---
+++ codemods/mocha/vitest/config-files/test/test.ts
@@ -0,0 +1,181 @@
+import {
+	FileSystemManager,
+	UnifiedFileSystem,
+	buildApi,
+	executeFilemod,
+} from '@intuita-inc/filemod';
+import { DirectoryJSON, Volume, createFsFromVolume } from 'memfs';
+import { Context } from 'mocha';
+import { deepStrictEqual, ok } from 'node:assert';
+import { repomod } from '../src/index.js';
+
+const transform = async (json: DirectoryJSON) => {
+	const volume = Volume.fromJSON(json);
+
+	const fileSystemManager = new FileSystemManager(
+		// @ts-expect-error type convergence
+		volume.promises.readdir,
+		volume.promises.readFile,
+		volume.promises.stat,
+	);
+	const unifiedFileSystem = new UnifiedFileSystem(
+		// @ts-expect-error type convergence
+		createFsFromVolume(volume),
+		fileSystemManager,
+	);
+
+	const api = buildApi(unifiedFileSystem, () => ({}), '/');
+
+	return executeFilemod(api, repomod, '/', {}, {});
+};
+
+describe('mocha config-files', function () {
+	const packageJsonPath = '/opt/project/package.json';
+	const packageJsonConfig = `
+    {
+      "name": "package-name",
+      "dependencies": {
+        "mocha": "^10.2.0",
+        "some-mocha-plugin": "^10.0.4"
+      },
+      "devDependencies": {
+        "mocha": "^10.2.0",
+        "@types/mocha": "^10.0.4"
+      },
+      "main": "./dist/index.cjs",
+      "types": "/dist/index.d.ts",
+      "scripts": {
+        "build:cjs": "cjs-builder ./src/index.ts",
+        "test": "mocha"
+      },
+      "mocha": {
+        "config-key": "config-value"
+      },
+      "files": [
+        "README.md",
+        "config.json",
+        "./dist/index.cjs",
+        "./index.d.ts"
+      ],
+      "type": "module"
+    }
+  `;
+
+	const tsconfigPath = '/opt/project/tsconfig.json';
+	const tsconfigContent = `
+    {
+      "compilerOptions": { "types": ["mocha"] },
+      "include": [
+        "./src/**/*.ts",
+        "./src/**/*.js",
+        "./test/**/*.ts",
+        "./test/**/*.js"
+      ]
+    }
+  `;
+
+	const mochaRcPath = '/opt/project/.mocharc';
+	const mochaRcCjsPath = '/opt/project/.mocharc.cjs';
+	const mochaConfigPath = '/opt/project/mocha.config.mjs';
+	const mochaRcContent = `
+    {
+      "loader": ["ts-node/esm"],
+      "full-trace": true,
+      "failZero": false,
+      "bail": true,
+      "spec": "./**/test.ts",
+      "timeout": 5000
+    }
+  `;
+
+	it('should contain correct file commands', async function (this: Context) {
+		const externalFileCommands = await transform({
+			[packageJsonPath]: packageJsonConfig,
+			[tsconfigPath]: tsconfigContent,
+			[mochaRcPath]: mochaRcContent,
+			[mochaRcCjsPath]: mochaRcContent,
+			[mochaConfigPath]: mochaRcContent,
+		});
+
+		deepStrictEqual(externalFileCommands.length, 5);
+
+		ok(
+			externalFileCommands.filter(
+				(command) =>
+					(command.kind === 'upsertFile' &&
+						command.path === packageJsonPath) ||
+					(command.kind === 'upsertFile' &&
+						command.path === tsconfigPath) ||
+					(command.kind === 'deleteFile' &&
+						command.path === mochaRcPath) ||
+					(command.kind === 'deleteFile' &&
+						command.path === mochaRcCjsPath) ||
+					(command.kind === 'deleteFile' &&
+						command.path === mochaConfigPath),
+			).length === externalFileCommands.length,
+		);
+	});
+
+	it('should correctly modify package and tsconfig jsons', async function (this: Context) {
+		const externalFileCommands = await transform({
+			[packageJsonPath]: packageJsonConfig,
+			[tsconfigPath]: tsconfigContent,
+			[mochaRcPath]: mochaRcContent,
+			[mochaRcCjsPath]: mochaRcContent,
+			[mochaConfigPath]: mochaRcContent,
+		});
+
+		ok(
+			externalFileCommands.some(
+				(command) =>
+					command.kind === 'upsertFile' &&
+					command.path === packageJsonPath &&
+					command.data.replace(/\W/gm, '') ===
+						`
+              {
+                "name": "package-name",
+                "dependencies": {},
+                "devDependencies": {
+                  "vitest": "^1.0.1",
+                  "@vitest/coverage-v8": "^1.0.1"
+                },
+                "main": "./dist/index.cjs",
+                "types": "/dist/index.d.ts",
+                "scripts": {
+                  "build:cjs": "cjs-builder ./src/index.ts",
+                  "test": "vitest run",
+                  "coverage": "vitest run --coverage"
+                },
+                "files": [
+                  "README.md",
+                  "config.json",
+                  "./dist/index.cjs",
+                  "./index.d.ts"
+                ],
+                "type": "module"
+              }
+            `.replace(/\W/gm, ''),
+			),
+		);
+
+		ok(
+			externalFileCommands.some(
+				(command) =>
+					command.kind === 'upsertFile' &&
+					command.path === tsconfigPath &&
+					command.data.replace(/\W/gm, '') ===
+						`
+              {
+                "compilerOptions": {},
+                "include": [
+                  "./src/**/*.ts",
+                  "./src/**/*.js",
+                  "./test/**/*.ts",
+                  "./test/**/*.js"
+                ]
+              }
+            `.replace(/\W/gm, ''),
+			),
+		);
+	});
+});
