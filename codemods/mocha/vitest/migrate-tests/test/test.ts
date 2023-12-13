36ba879420737c40a183bb04a17cfffc36133bbc
Benny Joo <sldisek783@gmail.com>
2023-12-13 10:02:18 +0000

    add 1 more test for migrate-tests codemod (#526)

--- codemods/mocha/vitest/migrate-tests/test/test.ts
+++ codemods/mocha/vitest/migrate-tests/test/test.ts
@@ -4,7 +4,7 @@ import transform from '../src/index.js';
 import { buildApi } from '@codemod-registry/utilities';
 
 describe('mocha/vitest test', function () {
-	it('test 1', function () {
+	it('when `expect` for `chai` is being imported', function () {
 		const INPUT = `
         import { expect } from 'chai';
 
@@ -33,6 +33,50 @@ describe('mocha/vitest test', function () {
         describe('Test Suite 2', () => {
           it('subtraction', () => {
             expect(1 - 1).to.equal(0);
+          });
+        });
+        `;
+
+		const fileInfo: FileInfo = {
+			path: 'index.ts',
+			source: INPUT,
+		};
+
+		const actualOutput = transform(fileInfo, buildApi('tsx'));
+
+		assert.deepEqual(
+			actualOutput?.replace(/\W/gm, ''),
+			OUTPUT.replace(/\W/gm, ''),
+		);
+	});
+
+	it('when `chai` is not used', function () {
+		const INPUT = `
+        describe('Test Suite 1', () => {
+          it('addition', () => {
+            assert(1 + 1 == 2);
+          });
+        });
+        
+        describe('Test Suite 2', () => {
+          it('subtraction', () => {
+            assert(1 - 1 == 0);
+          });
+        });
+        `;
+
+		const OUTPUT = `
+        import { describe, it } from 'vitest';
+
+        describe('Test Suite 1', () => {
+          it('addition', () => {
+            assert(1 + 1 == 2);
+          });
+        });
+        
+        describe('Test Suite 2', () => {
+          it('subtraction', () => {
+            assert(1 - 1 == 0);
           });
         });
         `;
