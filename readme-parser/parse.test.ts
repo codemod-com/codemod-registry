c642db19d82ac8fccffb582af039da182f512c68
Sasha <reactionnnnn@gmail.com>
2023-12-18 15:42:26 +0200

    feat: readme parser (#537)

---
+++ readme-parser/parse.test.ts
@@ -0,0 +1,185 @@
+import { describe, it } from 'vitest';
+import { convertToYaml, parse } from './parse.js';
+import { deepEqual } from 'assert';
+
+const DATA = `
+# Do the thing
+
+## Description
+
+This is an amazing codemod
+
+### WARNING
+
+This codemod does the thing
+
+## Example
+
+### Before
+
+\`\`\`ts
+http.get<ReqBodyType, PathParamsType>('/resource', (req, res, ctx) => {
+  return res(ctx.json({ firstName: 'John' }));
+});
+\`\`\`
+
+### After
+
+\`\`\`ts
+http.get<PathParamsType, ReqBodyType>('/resource', (req, res, ctx) => {
+  return res(ctx.json({ firstName: 'John' }));
+});
+\`\`\`
+
+### Before
+
+\`\`\`ts
+http.get<ReqBodyType>('/resource', (req, res, ctx) => {
+  return res(ctx.json({ firstName: 'John' }));
+});
+\`\`\`
+
+### After
+
+\`\`\`ts
+http.get<any, ReqBodyType>('/resource', (req, res, ctx) => {
+  return res(ctx.json({ firstName: 'John' }));
+});
+\`\`\`
+
+## Applicability Criteria
+
+\`MSW\` >= 1.0.0
+
+## Other Metadata
+
+### Codemod Version
+
+v1.0.0
+
+### Change Mode
+
+**Assistive**: The automation partially completes changes. Human involvement is needed to make changes ready to be pushed and merged.
+
+### **Codemod Engine**
+
+[ts-morph](https://github.com/dsherret/ts-morph)
+
+### Estimated Time Saving
+
+5 minutes per occurrence
+
+### Owner
+
+[Intuita](https://github.com/intuita-inc)
+
+### Links for more info
+
+- [Link1](https://example.com/)
+- [Link2](https://example1.com/)
+`;
+
+describe('parse/yaml', function () {
+	const parseResult = parse(DATA);
+
+	it('should parse correctly', async function () {
+		deepEqual(parseResult, {
+			name: 'Do the thing',
+			description:
+				'This is an amazing codemod\nWARNING\nThis codemod does the thing',
+			examples:
+				'Before\n' +
+				'```ts\n' +
+				"http.get<ReqBodyType, PathParamsType>('/resource', (req, res, ctx) => {\n" +
+				"  return res(ctx.json({ firstName: 'John' }));\n" +
+				'});\n' +
+				'```\n' +
+				'After\n' +
+				'```ts\n' +
+				"http.get<PathParamsType, ReqBodyType>('/resource', (req, res, ctx) => {\n" +
+				"  return res(ctx.json({ firstName: 'John' }));\n" +
+				'});\n' +
+				'```\n' +
+				'Before\n' +
+				'```ts\n' +
+				"http.get<ReqBodyType>('/resource', (req, res, ctx) => {\n" +
+				"  return res(ctx.json({ firstName: 'John' }));\n" +
+				'});\n' +
+				'```\n' +
+				'After\n' +
+				'```ts\n' +
+				"http.get<any, ReqBodyType>('/resource', (req, res, ctx) => {\n" +
+				"  return res(ctx.json({ firstName: 'John' }));\n" +
+				'});\n' +
+				'```',
+			applicability: 'MSW >= 1.0.0',
+			version: '1.0.0',
+			changeMode: 'assistive',
+			engine: 'ts-morph',
+			timeSave: '5 minutes/occurrence',
+			owner: 'Intuita',
+			links: 'https://example.com/,https://example1.com/',
+		});
+	});
+
+	it('should output correct YAML', async function () {
+		const yaml = convertToYaml(parseResult);
+
+		deepEqual(
+			yaml,
+			`
+---
+created-on: -
+f_long-description: |-
+  ## Description
+  This is an amazing codemod
+WARNING
+This codemod does the thing
+  Before
+\`\`\`ts
+http.get<ReqBodyType, PathParamsType>('/resource', (req, res, ctx) => {
+  return res(ctx.json({ firstName: 'John' }));
+});
+\`\`\`
+After
+\`\`\`ts
+http.get<PathParamsType, ReqBodyType>('/resource', (req, res, ctx) => {
+  return res(ctx.json({ firstName: 'John' }));
+});
+\`\`\`
+Before
+\`\`\`ts
+http.get<ReqBodyType>('/resource', (req, res, ctx) => {
+  return res(ctx.json({ firstName: 'John' }));
+});
+\`\`\`
+After
+\`\`\`ts
+http.get<any, ReqBodyType>('/resource', (req, res, ctx) => {
+  return res(ctx.json({ firstName: 'John' }));
+});
+\`\`\`
+f_github-link: -
+f_vs-code-link: -
+f_codemod-studio-link: -
+f_cli-command: -
+f_framework: -
+f_applicability-criteria: MSW >= 1.0.0
+f_verified-codemod: true
+f_author: -
+layout: "[automations].html"
+slug: -
+title: Do the thing
+updated-on: -
+published-on: -
+f_slug-name: -
+f_codemod-engine: cms/codemod-engines/ts-morph.md
+f_change-mode-2: Assistive
+f_estimated-time-saving: 5 minutes/occurrence
+tags: automations
+seo: -
+---
+`.trim(),
+		);
+	});
+});
---
+++ readme-parser/parse.ts
@@ -0,0 +1,313 @@
+import type { Heading, PhrasingContent, RootContent } from 'mdast';
+import { fromMarkdown } from 'mdast-util-from-markdown';
+
+const noFirstLetterLowerCase = (str: string) =>
+	str.length ? str[0] + str.slice(1).toLowerCase() : str;
+
+const capitalize = (str: string) =>
+	str[0] ? str[0].toUpperCase() + str.slice(1) : str;
+
+const getTextFromNode = (
+	node: RootContent | PhrasingContent | undefined,
+): string | null => {
+	if (!node) {
+		return null;
+	}
+
+	if ('value' in node) {
+		return node.value;
+	}
+
+	if ('children' in node) {
+		return getTextFromNode(node.children[0]);
+	}
+
+	return null;
+};
+
+const getUrlFromNode = (
+	node: RootContent | PhrasingContent | null,
+): string | null => {
+	if (!node) {
+		return null;
+	}
+
+	if ('url' in node) {
+		return node.url;
+	}
+
+	if ('children' in node) {
+		return getUrlFromNode(node.children[0] ?? null);
+	}
+
+	return null;
+};
+
+const getHeading = (
+	rootContents: ReadonlyArray<RootContent>,
+	depth: 1 | 2 | 3,
+	name: string | null,
+): Heading | null => {
+	for (const rootContent of rootContents) {
+		if (rootContent.type !== 'heading') {
+			continue;
+		}
+
+		if (name !== null) {
+			const headerTitle = getTextFromNode(rootContent);
+
+			if (
+				!headerTitle ||
+				!noFirstLetterLowerCase(headerTitle).startsWith(
+					noFirstLetterLowerCase(name),
+				)
+			) {
+				continue;
+			}
+		}
+
+		if (rootContent.depth === depth) {
+			return rootContent;
+		}
+	}
+
+	return null;
+};
+
+const getTextByHeader = (
+	rootContents: ReadonlyArray<RootContent>,
+	heading: Heading,
+	delimiter: string,
+) => {
+	const headerIndex = rootContents.findIndex(
+		(rc) => rc.position?.start.line === heading.position?.start.line,
+	);
+	const nextHeaderIndex = rootContents.findIndex(
+		(rc) =>
+			rc.type === 'heading' &&
+			rc.position?.start.line &&
+			heading.position?.start.line &&
+			rc.position?.start.line > heading.position?.start.line &&
+			rc.depth === heading.depth,
+	);
+
+	const contentParts = rootContents.slice(
+		headerIndex + 1,
+		nextHeaderIndex > -1 ? nextHeaderIndex : undefined,
+	);
+
+	const textParts: string[] = [];
+
+	for (const rc of contentParts) {
+		if ('children' in rc) {
+			rc.children
+				.map((child) => {
+					if (child.type === 'text') {
+						return `${child.value}${delimiter}`;
+					}
+
+					if (child.type === 'listItem') {
+						return `${getUrlFromNode(
+							child.children[0] ?? null,
+						)}${delimiter}`;
+					}
+
+					if (child.type === 'link' || child.type === 'strong') {
+						return getTextFromNode(child.children[0]);
+					}
+
+					// Do not add new line after certain blocks (treated as separate AST nodes)
+					if (child.type === 'inlineCode') {
+						return child.value;
+					}
+
+					return null;
+				})
+				.forEach((child) => {
+					if (child !== null) {
+						textParts.push(child);
+					}
+				});
+		}
+
+		if ('value' in rc) {
+			if (rc.type === 'code') {
+				textParts.push(
+					`\`\`\`${rc.lang}\n${rc.value}\n\`\`\`${delimiter}`,
+				);
+			} else {
+				textParts.push(`${rc.value}${delimiter}`);
+			}
+		}
+	}
+
+	// Trim last el to remove delimiter
+	textParts[textParts.length - 1] =
+		textParts.at(-1)?.replace(/(\W)$/, '') ?? '';
+
+	return textParts.join('');
+};
+
+export const parse = (data: string) => {
+	const { children } = fromMarkdown(data);
+
+	const nameHeading = getHeading(children, 1, null);
+	const name =
+		nameHeading?.children[0] && 'value' in nameHeading.children[0]
+			? nameHeading.children[0].value
+			: null;
+
+	if (!name) {
+		throw new Error('Name not found');
+	}
+
+	const descHeading = getHeading(children, 2, 'Description');
+	const description = descHeading
+		? getTextByHeader(children, descHeading, '\n')
+		: null;
+	if (!description) {
+		throw new Error('Description not found');
+	}
+
+	const exampleHeading = getHeading(children, 2, 'Example');
+	const examples = exampleHeading
+		? getTextByHeader(children, exampleHeading, '\n')
+		: null;
+	if (!examples) {
+		throw new Error('Examples not found');
+	}
+
+	const applicabilityHeader = getHeading(children, 2, 'Applicability');
+	const applicability = applicabilityHeader
+		? getTextByHeader(children, applicabilityHeader, '\n')
+		: null;
+	if (!applicability) {
+		throw new Error('Applicability criteria not found');
+	}
+	if (!applicability.match(/[\w]+ (>|>=) \d+\.\d+\.\d+/)) {
+		throw new Error('Applicability criteria is of a wrong format');
+	}
+
+	const versionHeader = getHeading(children, 3, 'Codemod Version');
+	const version = versionHeader
+		? getTextByHeader(children, versionHeader, '\n')
+		: null;
+	if (!version) {
+		throw new Error('Codemod version not found');
+	}
+	const versionMatch = version.match(/(v)?(\d+\.\d+\.\d+)/)?.at(2);
+
+	const changeModeHeader = getHeading(children, 3, 'Change Mode');
+	const changeModeText = changeModeHeader
+		? getTextByHeader(children, changeModeHeader, '\n').toLowerCase()
+		: null;
+	if (!changeModeText) {
+		throw new Error('Change mode not found');
+	}
+	let changeMode: string | null = null;
+	if (changeModeText.includes('assistive')) {
+		changeMode = 'assistive';
+	} else if (changeModeText.includes('autonomous')) {
+		changeMode = 'autonomous';
+	}
+	if (!changeMode) {
+		throw new Error('Change mode is of a wrong format');
+	}
+
+	const engineHeader = getHeading(children, 3, 'Codemod Engine');
+	const engineText = engineHeader
+		? getTextByHeader(children, engineHeader, '\n')
+		: null;
+	if (!engineText) {
+		throw new Error('Codemod engine not found');
+	}
+	let engine: string | null = null;
+	if (engineText.includes('ts-morph')) {
+		engine = 'ts-morph';
+	} else if (engineText.includes('jscodeshift')) {
+		engine = 'jscodeshift';
+	}
+	if (!engine) {
+		throw new Error('Codemod engine is of a wrong format');
+	}
+
+	const timeSaveHeader = getHeading(children, 3, 'Estimated Time Saving');
+	const timeSave = timeSaveHeader
+		? getTextByHeader(children, timeSaveHeader, '\n').replace(' per ', '/')
+		: null;
+	if (!timeSave) {
+		throw new Error('Estimated time saving not found');
+	}
+
+	const ownerHeader = getHeading(children, 3, 'Owner');
+	const owner = ownerHeader
+		? getTextByHeader(children, ownerHeader, '\n') ?? 'Intuita'
+		: null;
+
+	const linksHeader = getHeading(children, 3, 'Links');
+	const links = linksHeader
+		? getTextByHeader(children, linksHeader, ',')
+		: null;
+
+	return {
+		name,
+		description,
+		examples,
+		applicability,
+		version: versionMatch,
+		changeMode,
+		engine,
+		timeSave,
+		owner,
+		links,
+	};
+};
+
+export const convertToYaml = (data: ReturnType<typeof parse>) => {
+	const {
+		name,
+		description,
+		examples,
+		applicability,
+		changeMode,
+		engine,
+		timeSave,
+		owner,
+	} = data;
+
+	// f_author - possible to find by .includes(<owner>) under cms/authors, like cms/authors/rajasegar-chandran.md
+	// f_slug_name - can infer from config by throwing away framework version?
+	// f_cli_command - intuita <infer_name_from_config>
+	// f_github_link - <should_be_available_in_push_metadata>
+	// create-on - <readme_file_create_time>
+	const res = `
+---
+created-on: -
+f_long-description: |-
+  ## Description
+  ${description}
+  ${examples}
+f_github-link: -
+f_vs-code-link: -
+f_codemod-studio-link: -
+f_cli-command: -
+f_framework: -
+f_applicability-criteria: ${applicability}
+f_verified-codemod: ${owner === 'Intuita' ? 'true' : 'false'}
+f_author: -
+layout: "[automations].html"
+slug: -
+title: ${name}
+updated-on: -
+published-on: -
+f_slug-name: -
+f_codemod-engine: cms/codemod-engines/${engine}.md
+f_change-mode-2: ${capitalize(changeMode)}
+f_estimated-time-saving: ${timeSave}
+tags: automations
+seo: -
+---
+`.trim();
+
+	return res;
+};
