f2edd38733977f790176b40ebf5fd91336874062
Sasha Mysak <reactionnnnn@gmail.com>
2023-12-21 16:17:14 +0100

    fix(parser): use origin/main~1

--- readme-parser/sync.ts
+++ readme-parser/sync.ts
@@ -49,7 +49,7 @@ export const sync = async () => {
 	await git.fetch(['website', 'master']);
 	await git.fetch(['origin', 'main']);
 
-	const diff = await git.diff(['--name-only', 'HEAD~1']);
+	const diff = await git.diff(['--name-only', 'origin/main~1']);
 	const readmesChanged = diff
 		.split('\n')
 		.filter((path) => path.match(/^codemods\/.*README\.md$/));
@@ -77,7 +77,7 @@ export const sync = async () => {
 		}
 
 		try {
-			oldFile = await git.catFile(['-p', `HEAD~1:${path}`]);
+			oldFile = await git.catFile(['-p', `origin/main~1:${path}`]);
 		} catch (err) {
 			oldFile = null;
 		}
